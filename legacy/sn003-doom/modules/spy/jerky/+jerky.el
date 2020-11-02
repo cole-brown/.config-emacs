6278c9d366929d3a32825739436a9c1738264911
Cole Brown <code@brown.dev>
2020-11-02 15:53:07 -0800

    Updated jerky to support hash table too.
    Currently can toggle between hash table and alist tree.
      - Will delete feature toggle and alist code next.

--- legacy/sn003-doom/modules/spy/jerky/+jerky.el
+++ legacy/sn003-doom/modules/spy/jerky/+jerky.el
@@ -14,7 +14,7 @@
 ;; §-TODO-§ [2020-10-23]: Make this its own package.
 
 ;;------------------------------------------------------------------------------
-;; Consts & Vars
+;; Jerky's Repository of... dried meats?
 ;;------------------------------------------------------------------------------
 
 ;; TODO: Change this to a hash table?
@@ -41,6 +41,59 @@ I'm not your mother.
 ")
 
 
+(defun jerky//repo/test/string=/cmp (a b)
+  "Compare strings (ignoring case) for `jerky//repo/test/string='.
+"
+  ;; `compare-strings' returns t or integer.
+  ;; Convert that to a bool t/nil.
+  (eq t
+      ;; Compare the strings, ignoring case.
+      (compare-strings a nil nil b nil nil t)))
+
+
+(defun jerky//repo/test/string=/hash (key)
+  "Get a hashed value for the (lowercased) key for
+`jerky//repo/test/string='.
+"
+  (sxhash-equal (downcase key)))
+
+
+;; String comparison, ignores case.
+(define-hash-table-test 'jerky//repo/test/string=
+  'jerky//repo/test/string=/cmp
+  'jerky//repo/test/string=/hash)
+
+
+(defvar jerky//repo (make-hash-table :test 'jerky//repo/test/string=
+                                     :weakness nil)
+  "A key-path/value store, basically.
+
+Each entry is a list. There are three cells like so:
+'(key values docstr)
+
+KEY should be the key it's stored under (for inspection).
+
+VALUES is a plist of 'namespace' to value. e.g.
+  '(:default \"/mnt/jeff/dir\" :home \"~/my-stuff/jeff/dir\")
+
+  - `:default' is special - it is the fallback namespace if not found in the
+    desired namespace, or the namespace if none is specified.
+  - Actual values in the plist can be whatever.
+
+DOCSTR should be short.
+  - Or not.
+    - I'm not your mother.
+")
+
+;; Are we testing the hash-table change, or using the alist?
+;; (setq jerky//feature/repo nil)
+(setq jerky//feature/repo t)
+
+
+;;------------------------------------------------------------------------------
+;; Customizables and Constants
+;;------------------------------------------------------------------------------
+
 (defcustom jerky/custom:keys/separator "/"
   "Jerky keys will be split/joined using this character.
 
@@ -53,7 +106,7 @@ E.g.: (jerky/get 'my/key :path \"to/dir\")
 ;; Keys Functions
 ;;------------------------------------------------------------------------------
 
-(defalias 'jerky//keys/symbol->str 'spy/string/symbol-name->str
+(defalias 'jerky//keys/symbol->str 'spy/string/symbol->str
   "Convert a symbol to a string. Removes ':' from keywords.")
 
 
@@ -67,7 +120,8 @@ E.g.: (jerky/get 'my/key :path \"to/dir\")
 (jerky//keys/normalize \"a\" b c)
   -> (a b c)
 "
-  (let ((keys '())
+  (let ((keys '()) ; alist tree
+        (key  nil) ; hash table
         (strings '()))
     (dolist (arg args)
       ;; Push string args to strings, turn non-strings into strings.
@@ -90,6 +144,11 @@ E.g.: (jerky/get 'my/key :path \"to/dir\")
                     arg))))
 
     ;; Now we have strings. They are in backwards order. They need to be turned
+    ;; into a final separated string.
+    (setq key (s-join jerky/custom:keys/separator
+                      (nreverse strings)))
+
+    ;; Now we have strings. They are in backwards order. They need to be turned
     ;; into a final separated string so we can then break it apart into final key
     ;; list... One of the args could've been a compond key - e.g. "jeff/key".
     (dolist (name
@@ -97,14 +156,19 @@ E.g.: (jerky/get 'my/key :path \"to/dir\")
              (s-split (concat "["
                               jerky/custom:keys/separator
                               "]")
-                      (s-join jerky/custom:keys/separator
-                              (nreverse strings))))
+                     key))
+
       ;; Make symbols out of the key strings.
       (push (intern name) keys))
 
-    ;; Now we have a backwards list... again.
-    ;; Turn it into a forwards list.
-    (nreverse keys)))
+    ;; Return the full key string if doing hash tables, else return the list of symbols.
+    (if jerky//feature/repo
+        ;; Key string is good as-is.
+        key
+
+     ;; Keys list is a backwards list... again.
+     ;; Turn it into a forwards list.
+     (nreverse keys))))
 ;; (jerky//keys/normalize "a/b" "c")
 ;; (jerky//keys/normalize :base "a/b" "c")
 
@@ -133,48 +197,53 @@ For all others, this returns t to indicate KEY is a valid/desired key.
 ;;------------------------------------------------------------------------------
 
 
-(defun jerky//write/update (kvs keys &optional value docstr)
-  "Looks in KVS-SYMBOL for KEYS. Updates final key in KEYS to:
+(defun jerky//write/update (repo keys &optional value docstr)
+  "Looks in jerky's key-value REPO for KEYS. Updates final key in KEYS to:
 (list KEY VALUE DOCSTR)
 
-KEYS should be a list in forwards/correct order. E.g.: '(path to the thing)
+KEYS should be the output of `jerky//keys/normalize'.
 
-KVS-SYMBOL should be the (quoted) symbol. E.g.: 'jerky//kvs
+REPO should be the (quoted) symbol. E.g.: 'jerky//kvs
 
 VALUE can be whatever.
 
 DOCSTR can be nil or a string, but is not verified.
 "
-  (let ((key (car keys))
-        (rest (cdr keys)))
+  (if jerky//feature/repo
+      ;; Write to the hash table:
+      (puthash keys (list keys value docstr) jerky//repo)
 
-    (let ((entry (assoc key kvs)))
-      (if (null entry)
-          ;; Entry doesn't exist; push a new one into the list.
+    ;; Else, write to the KVS tree.
+    (let ((key (car keys))
+          (rest (cdr keys)))
+
+      (let ((entry (assoc key repo)))
+        (if (null entry)
+            ;; Entry doesn't exist; push a new one into the list.
+            (if (null rest)
+                ;; This is the end; we know what to do.
+                (push (list key value docstr) repo)
+
+              ;; Have `jerky//write/tree' take care of the rest when
+              ;; this isn't the end...
+              (push ;; (list ;; The alist...
+               ;; The alist's key's entry... Put `key' and `rest' back
+               ;; together so it can deal with as much as possible.
+               (jerky//write/tree (cons key rest) value docstr) ;;)
+               repo))
+
+          ;; Entry does exist; update cdr slot with new list.
           (if (null rest)
               ;; This is the end; we know what to do.
-              (push (list key value docstr) kvs)
+              (setf (cdr entry) (cons value docstr))
 
-            ;; Have `jerky//write/tree' take care of the rest when
-            ;; this isn't the end...
-            (push ;; (list ;; The alist...
-                   ;; The alist's key's entry... Put `key' and `rest' back
-                   ;; together so it can deal with as much as possible.
-                   (jerky//write/tree (cons key rest) value docstr) ;;)
-                  kvs))
-
-        ;; Entry does exist; update cdr slot with new list.
-        (if (null rest)
-            ;; This is the end; we know what to do.
-            (setf (cdr entry) (cons value docstr))
-
-          ;; We don't know what to do; but maybe if we ask ourself?..
-          (setf (nth 1 entry) (jerky//write/update (nth 1 entry)
-                                                          rest
-                                                          value
-                                                          docstr))))))
-  ;; Always return the (modified) key-value store.
-  kvs)
+            ;; We don't know what to do; but maybe if we ask ourself?..
+            (setf (nth 1 entry) (jerky//write/update (nth 1 entry)
+                                                     rest
+                                                     value
+                                                     docstr))))))
+    ;; Always return the (modified) key-value store.
+    repo))
 ;; (setq spy//test/jerky nil)
 ;; (symbol-value 'spy//test/jerky)
 ;; (jerky//write/update spy//test/jerky '(path) "test" "docstr")
@@ -187,6 +256,9 @@ DOCSTR can be nil or a string, but is not verified.
   "Uses the list of KEYS to make a tree of empty branches out to the final leaf
 of (key VALUE DOCSTR).
 "
+  (when jerky//feature/repo
+    (error "'Write a subtree' not supported for storage backend of hash table."))
+
   (if (cdr keys)
       ;; This is an alist of alists, so we need the cdr to be that 'of alist'...
       ;; So we put the list we get back from ourself into a list. It'll end up
@@ -230,10 +302,13 @@ If not provided, they will be nil.
          ((&plist :docstr docstr :value value)
           (-drop-while #'jerky//keys/desired? args)))
 
-    (setq jerky//kvs (jerky//write/update jerky//kvs
-                                                        keys
-                                                        value
-                                                        docstr))))
+    (if jerky//feature/repo
+        (jerky//write/update jerky//kvs keys value docstr)
+
+      (setq jerky//kvs (jerky//write/update jerky//kvs
+                                            keys
+                                            value
+                                            docstr)))))
 ;; jerky//kvs
 ;; (setq jerky//kvs nil)
 ;; (jerky/set 'path 'to 'thing :value "hello there")
@@ -246,8 +321,29 @@ If not provided, they will be nil.
 ;; Reading from Key-Value Store
 ;;------------------------------------------------------------------------------
 
+(defun jerky//get/key (entry)
+  "Get the key of this entry in jerky's repo.
+"
+  ;; key is at index 0
+  (nth 0 entry))
+
+
+(defun jerky//get/value (entry)
+  "Get the value of this entry in jerky's repo.
+"
+  ;; value is at index 1
+  (nth 1 entry))
+
+
+(defun jerky//get/docstr (entry)
+  "Get the docstr of this entry in jerky's repo.
+"
+  ;; docstr is at index 2
+  (nth 2 entry))
+
+
 (defun jerky/get (&rest keys)
-  "Gets an entry from `jerky//keys'.
+  "Gets an entry's value from `jerky//keys'.
 
 KEYS: Key path to walk down to find value to return.
 
@@ -258,9 +354,14 @@ If nothing found at KEYS, return will be nil.
   (let ((keys (apply #'jerky//keys/normalize keys))
         (kvs jerky//kvs))
 
-    (dolist (key keys)
-      (setq kvs (nth 1 (assoc key kvs))))
-    kvs))
+    (if jerky//feature/repo
+        ;; Return keys' value or null.
+        (jerky//get/value (gethash keys jerky//repo))
+
+      ;; Else, walk our tree to find the key.
+      (dolist (key keys)
+        (setq kvs (nth 1 (assoc key kvs))))
+      kvs)))
 ;; (setq jerky//kvs nil)
 ;; (setq jerky//kvs '((test ((jill "jill" nil) (jeff "jeffe" "I am a comment."))) (path ((to ((thing "hello there" nil))))))
 ;; jerky//kvs
