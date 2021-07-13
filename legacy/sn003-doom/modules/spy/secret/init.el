3a952156c3988b4fdbf388faa69372563584ceec
Cole Brown <git@spydez.com>
2021-07-13 14:38:26 -0700

    Refactor ':spy/secret' in prep for better init.
    Preparing for a better init experience for cases like:
      - New system without secrets yet.
      - Secrets init breaks.
    
    Made functions:
      - spy:secret/hash
      - spy:secret/id
    
    Currently they just give back the same info as their ':spy/system'
    counterparts.
    
    Made fuction `sss:secret/path' so that error checking is centralized.
      - `spy:secret/init', `spy:secret/config', and `sss:secret/load' all
        use this now.
      - New `spy:secret/has' also uses this to check for existance of
        secrets' init.el file.
    
    New predicate function `spy:secret/has' for checking if this system has
    secrets.
    
    New macro `spy:secret/if' for running a block of code only if this
    system has secrets.
      - Not used anywhere yet, but that is up next.
    
    Updated docstr for `sss:secret/load.path' to reflect how only secrets
    files should be using it.

--- legacy/sn003-doom/modules/spy/secret/init.el
+++ legacy/sn003-doom/modules/spy/secret/init.el
@@ -36,6 +36,168 @@
 
 
 ;;------------------------------------------------------------------------------
+;; Getters
+;;------------------------------------------------------------------------------
+
+(defun spy:secret/hash ()
+  "Get this system's secret hash, which is the same as the system's hash."
+  (spy:system/hash))
+;; (spy:secret/hash)
+;; (string= (spy:secret/hash) (spy:system/hash))
+
+
+(defun spy:secret/id ()
+  "Get this system's secrets ID.
+
+Returns nil if no secrets ID for this system."
+  (spy:system/get (spy:system/hash) 'id))
+;; (spy:secret/id)
+
+
+(defun sss:secret/path (file)
+  "Get the path to secrets FILE for this system.
+
+NOTE FILE must be filename sans extension (e.g. \"init\", not \"init.el\")!
+
+Returns a plist with keys:
+  - :id        - `spy:secret/id' string or nil.
+  - :path/name - Path to FILE, with extension.
+  - :path/load - Path to FILE, without extension. Use this for loading the file. "
+  (if-let* ((hash (spy:secret/hash))
+            (id   (spy:secret/id))
+            (dir  (spy:system/get hash 'path 'secret 'system))
+            (path (spy:path/to-file dir file)) ; No ".el"; want compiled too.
+            (name (concat path ".el")))
+      (progn
+        (mis0/init/message (mapconcat #'identity
+                            '("sss:secret/path(%S)"
+                              "  hash: %S"
+                              "  id:   %S"
+                              "  dir:  %S"
+                              "  path: %S"
+                              "  name: %S")
+                            "\n")
+                 file hash id dir path name)
+
+
+        ;; We got all the vars from jerky, so check for existance now.
+        ;;    Do we have valid-ish data to check?
+        (cond ((or (null dir)
+                   (not (stringp dir)))
+               (mis0/init/message
+                (concat "%s(%s): "
+                        "Secrets %s for this system (%s) cannot be determined; "
+                        "directory is not a string: %s")
+                "sss:secret/path"
+                file
+                "directory"
+                id
+                dir)
+
+               ;; TODO: warn?
+
+               ;; Return a 'does not exist'.
+               (list :id id :file-name nil :load-path nil))
+
+              ;; Does dir even exist?
+              ((not (file-directory-p dir))
+               (mis0/init/message
+                (concat "%s(%s): "
+                        "Secrets %s for this system (%s) does not exist: %s")
+                "sss:secret/path"
+                file
+                "directory"
+                id
+                dir)
+
+               ;; TODO: warn?
+
+               ;; Return a 'does not exist'.
+               (list :id id :file-name nil :load-path nil))
+
+              ;; What about the filepath?
+              ;; Add ".el" for actual file check.
+              ((not (file-exists-p name))
+               (mis0/init/message
+                (concat "%s(%s): "
+                        "Secrets %s for this system (%s) does not exist: %s")
+                "sss:secret/path"
+                file
+                "file"
+                id
+                name)
+
+               ;; TODO: warn?
+
+               ;; Return a 'does not exist'.
+               (list :id id :file-name nil :load-path nil))
+
+              ;; File exists; return the load path...
+              (t
+               (list :id id :file-name name :load-path path))))
+
+    ;; Else no hash or id or dir found...
+    (mis0/init/message (mapconcat #'identity
+                                  '("sss:secret/load:"
+                                    "  No secret '%s' for this system."
+                                    "  file: %s"
+                                    "  -> hash: %s"
+                                    "  ->   id: %s"
+                                    "  ->  dir: %s"
+                                    "  -> path: %s"
+                                    "  -> name: %s")
+                               "\n")
+                       key file
+                       hash id dir path name)
+
+    ;; TODO: warn?
+
+    ;; Return a 'does not exist'.
+    (list :id id :file-name nil :load-path nil)))
+
+
+(defun spy:secret/has ()
+  "Returns non-nil if this system has secrets.
+
+The system is considered to have secrets if:
+  - It has a hash.
+  - It has an ID.
+  - And it has a secrets 'init.el' file."
+  (condition-case _
+      (not (null (sss:secret/path "init")))
+    (error nil)))
+;; (spy:secret/has)
+
+
+;;------------------------------------------------------------------------------
+;; Configuration Helpers
+;;------------------------------------------------------------------------------
+
+
+(defmacro spy:secret/if (func-or-place skip-message &rest body)
+  "Do BODY if this system has secrets.
+
+Used to guard config blocks that depend on secrets.
+
+FUNC-OR-PLACE and SKIP-MESSAGE are used only if no secrets. They are formatted
+into a `mis0/init/message' for info about skipped config.
+  - If SKIP-MESSAGE is a list, it will be passed through `format'."
+  ;; Only eval inputs once.
+  `(let ((mmm:func-or-place ,func-or-place)
+         (mmm:skip-message ,skip-message))
+     (if (spy:secret/has)
+         ,@body
+       (mis0/init/message (concat "\n|%s| %s: %s")
+                          (or mmm:func-or-place
+                              "spy:secret/if (unlabeled)")
+                          "[SKIP]"
+                          (if (listp mmm:skip-message)
+                              (apply #'format mmm:skip-message)
+                            mmm:skip-message)))))
+;; (spy:secret/if "testing" "skip string" (message "I should not see this."))
+
+
+;;------------------------------------------------------------------------------
 ;; The End.
 ;;------------------------------------------------------------------------------
 (imp:provide :modules 'spy 'secret)
--- legacy/sn003-doom/modules/spy/secret/load.el
+++ legacy/sn003-doom/modules/spy/secret/load.el
@@ -18,75 +18,18 @@
 
 (defun spy:secret/init ()
   "Load secret's root init.el."
-  (let* ((hash (spy:system/hash))
-         (id   (spy:system/get hash 'id))
-         (file/name "init")
-         (file/ext ".el")
-         (secret/path/emacs (spy:system/get hash 'path 'secret 'emacs))
-         ;; No file extension so we can load compiled if it exists.
-         (secret/path/load (concat secret/path/emacs file/name))
-         ;; With file extension so we can check that file exists.
-         (secret/path/file (concat secret/path/load file/ext)))
+  (if (spy:secret/has)
+      (sss:secret/load "init")
 
-    ;; Check that we have a valid secret to load.
-    (cond ((or (null hash)
-               (null id))
-           (mis0/init/message (concat "%S %S: "
-                                      "No secret hash and/or id:\n"
-                                      "hash: %S\n"
-                                      "id:   %S")
-                              "|secret|"
-                              "[SKIP]"
-                              hash
-                              id))
-
-          ;; Found a path to use?
-          ((or (null secret/path/emacs)
-               (not (stringp secret/path/emacs)))
-           (mis0/init/message "Secrets %s for this system (%s) cannot be determined; directory is not a string: %s"
-                              "root emacs directory"
-                              id secret/path/emacs)
-           ;; TODO: no warn? Just use mis0?
-           (warn "Secrets %s for this system (%s) cannot be determined; directory is not a string: %s"
-                 "root emacs directory"
-                 id secret/path/emacs)
-           nil)
-
-          ;; Does path even exist?
-          ((not (file-directory-p secret/path/emacs))
-           (mis0/init/message "Secrets %s for this system (%s) does not exist: %s"
-                              "root emacs directory"
-                              id secret/path/emacs)
-           ;; TODO: no warn? Just use mis0?
-           (warn "Secrets %s for this system (%s) does not exist: %s"
-                 "root emacs directory"
-                 id secret/path/emacs)
-           nil)
-
-          ;; What about the filepath?
-          ((not (file-exists-p secret/path/file))
-           (mis0/init/message "Secrets %s for this system (%s) does not exist: %s"
-                              (concat "root emacs '" file/name file/ext "'")
-                              id secret/path/file)
-           ;; TODO: no warn? Just use mis0?
-           (warn "Secrets %s for this system (%s) does not exist: %s"
-                 (concat "root emacs '" file/name file/ext "'")
-                 id secret/path/file)
-           nil)
-
-          ;; File exists; load it...
-          (t
-           (mis0/init/message "Loading %s for %s...\n   load path: %s"
-                              (concat "root emacs '" file/name "'")
-                              id
-                              secret/path/load)
-           (message "%s: Loading %s for %s...\n   load path: %s"
-                    "|secret|"
-                    (concat "root emacs '" file/name "'")
-                    id
-                    secret/path/load)
-           ;; TODO: no message? Just use mis0?
-           (load secret/path/load)))))
+    ;; No secrets for this system.
+    (mis0/init/message (concat "%S %S: "
+                               "No secret hash, id, or 'init.el':\n"
+                               "  hash: %S\n"
+                               "  id:   %S")
+                       "|spy:secret/init|"
+                       "[SKIP]"
+                       (spy:secret/hash)
+                       (spy:secret/id))))
 
 
 ;;------------------------------------------------------------------------------
@@ -95,107 +38,47 @@
 
 (defun spy:secret/config ()
   "Configure this system's secrets."
-  (sss:secret/load 'emacs "config"))
+  (if (spy:secret/has)
+      (sss:secret/load "config")
+
+    ;; No secrets for this system.
+    (mis0/init/message (concat "%S %S: "
+                               "No secret hash, id, or 'init.el':\n"
+                               "  hash: %S\n"
+                               "  id:   %S")
+                       "|spy:secret/config|"
+                       "[SKIP]"
+                       (spy:secret/hash)
+                       (spy:secret/id))))
 
 
 ;;------------------------------------------------------------------------------
 ;; Helper Functions for Loading Files
 ;;------------------------------------------------------------------------------
 
-(defun sss:secret/load (key file)
+(defun sss:secret/load (file)
   "Load FILE (do not include '.el[c]') from this system's secrets
-directory indicated by KEY, if it has secrets.
+directory indicated by KEY, if it has secrets."
+  (let* ((results (sss:secret/path file))
+         (id (plist-get results :id))
+         (path/name (plist-get results :path/file))
+         (path/load (plist-get results :path/load)))
 
-It must have these keys in jerky:
-  hash: 'system 'hash
-  id:   'system 'secret 'identities <hash>
-  dir:  'system 'path 'secret KEY
-
-And it must have FILE in <dir>.
-"
-  ;; Load our specific secrets if we have the system set up for it.
-  (if-let* ((hash (spy:system/hash))
-            (id   (spy:system/get hash 'id))
-            (dir  (spy:system/get hash 'path 'secret 'system))
-            (path (spy:path/to-file dir file)) ; No ".el"; want compiled too.
-            (name (concat path ".el")))
-      (progn
-        (mis0/init/message (mapconcat #'identity
-                            '("sss:secret/load(%S %S)"
-                              "  hash: %S"
-                              "  id:   %S"
-                              "  dir:  %S"
-                              "  path  %S"
-                              "  name: %S")
-                            "\n")
-                 key file hash id dir path name)
-
-        ;; We got all the vars from jerky, so check for existance now.
-        ;;    Do we have valid-ish data to check?
-        (cond ((or (null dir)
-                   (not (stringp dir)))
-               (mis0/init/message "Secrets %s for this system (%s) cannot be determined; directory is not a string: %s"
-                        "directory"
-                        id dir)
-               (warn "Secrets %s for this system (%s) cannot be determined; directory is not a string: %s"
-                     "directory"
-                     id dir)
-               nil)
-
-              ;; Does dir even exist?
-              ((not (file-directory-p dir))
-               (mis0/init/message "Secrets %s for this system (%s) do not exist: %s"
-                        "directory"
-                        id dir)
-               (warn "Secrets %s for this system (%s) do not exist: %s"
-                     "directory"
-                     id dir)
-               nil)
-
-              ;; What about the filepath?
-              ;; Add ".el" for actual file check.
-              ((not (file-exists-p name))
-               (mis0/init/message "Secrets %s for this system (%s) does not exist: %s"
-                        "file"
-                        id path)
-               (warn "Secrets %s for this system (%s) does not exist: %s"
-                     "file"
-                     id path)
-               nil)
-
-              ;; File exists; load it...
-              (t
-               (mis0/init/message "Loading %s secrets...\n   %s" id name)
-               (message "SECRETS IS LOADING... %S" path )
-               ;; TODO: no message? use mis0 or something?
-               (load path))))
-
-    ;; Else no hash or id or dir found...
-    ;; TODO: warning? quiet? use mis0 or something?
-    (mis0/init/message (mapconcat #'identity
-                                  '("sss:secret/load:"
-                                    "  No secret '%s' for this system."
-                                    "  file: %s"
-                                    "  -> hash: %s"
-                                    "  ->   id: %s"
-                                    "  ->  dir: %s"
-                                    "  -> path: %s"
-                                    "  -> name: %s")
-                               "\n")
-                       key file
-                       hash id dir path name))
-  nil)
+    ;; `sss:secret/path' took care of error cases, so just load if not nil.
+    (when path/load
+      (mis0/init/message "Loading %s secrets...\n   %s" id path/name)
+      (load path/load))))
 
 
 (defun sss:secret/load.path (&rest path)
-  "Attempts to load file rooted at jerky key:
-  - 'system <hash> 'path 'secret 'emacs
+  "For use in secrets files themselves, to load sub-files.
 
-Appends PATH (do not include '.el[c]' in the last, filename, component).
-"
-  (if-let* ((hash (spy:system/hash))
-            (id   (spy:system/get hash 'id))
-            (root (spy:system/get (spy:system/hash) 'path 'secret 'emacs))
+Attempts to load file at system's 'path/secret/init' as defined by ':spy/system'.
+
+NOTE: Do not include file's extension ('.el[c]') in PATH."
+  (if-let* ((hash (spy:secret/hash))
+            (id   (spy:secret/id))
+            (root (spy:system/get hash 'path 'secret 'emacs))
             (filepath (apply #'spy:path/to-file root path)) ; No ".el"; want compiled too.
             (name (concat filepath ".el")))
 
