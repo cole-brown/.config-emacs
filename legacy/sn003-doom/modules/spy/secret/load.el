6e398e93f00bec63e3b9a6f2da00b8123d10330e
Cole Brown <git@spydez.com>
2021-07-16 14:09:40 -0700

    Moved functions to be below functions they use.

--- legacy/sn003-doom/modules/spy/secret/load.el
+++ legacy/sn003-doom/modules/spy/secret/load.el
@@ -7,76 +7,8 @@
 
 (imp:require :jerky)
 (imp:require :modules 'spy 'file 'path)
-(imp:require :modules 'spy 'system)
 
 (require 'mis0/message)
-
-
-;;------------------------------------------------------------------------------
-;; Initialization
-;;------------------------------------------------------------------------------
-
-;; TODO: move down below
-(defun spy:secret/init ()
-  "Load secret's root init.el."
-  (if (spy:secret/has)
-      (sss:secret/load.root "init")
-
-    ;; No secrets for this system.
-    (let ((path/init (spy:path/to-file (sss:secret/path/system) "init.el")))
-      (mis0/init/warning "spy:secret/init"
-                         (mapconcat #'identity
-                                    '("%s: Cannot init; system has no secrets."
-                                      "  hash: %s"
-                                      "  ID:   %s"
-                                      "  init: %s"
-                                      "   - exists?: %s")
-                                    "\n")
-                         "[SKIP]"
-                         (spy:secret/hash)
-                         (spy:secret/id)
-                         path/init
-                         (if (file-exists-p path/init)
-                             "yes"
-                           "no")))))
-;; (spy:secret/init)
-
-
-;;------------------------------------------------------------------------------
-;; Configuration
-;;------------------------------------------------------------------------------
-
-;; TODO: move down below
-(defun spy:secret/config ()
-  "Configure this system's secrets."
-  (if (spy:secret/has)
-      (sss:secret/load.root "config")
-
-    ;; No secrets for this system.
-    (let ((path/init (spy:path/to-file (sss:secret/path/system) "init.el"))
-          (path/config (spy:path/to-file (sss:secret/path/system) "config.el")))
-      (mis0/init/warning "spy:secret/init"
-                         (mapconcat #'identity
-                                    '("%s: Cannot init; system has no secrets."
-                                      "  hash:   %s"
-                                      "  ID:     %s"
-                                      "  init:   %s"
-                                      "   - exists?: %s"
-                                      "  config: %s"
-                                      "   - exists?: %s")
-                                    "\n")
-                         "[SKIP]"
-                         (spy:secret/hash)
-                         (spy:secret/id)
-                         path/init
-                         (if (file-exists-p path/init)
-                             "yes"
-                           "no")
-                         path/config
-                         (if (file-exists-p path/config)
-                             "yes"
-                           "no")))))
-;; (spy:secret/config)
 
 
 ;;------------------------------------------------------------------------------
@@ -207,7 +139,69 @@ NOTE: Do not include file's extension ('.el[c]') in PATH."
   nil)
 
 
+;;------------------------------------------------------------------------------
+;; Initialization
+;;------------------------------------------------------------------------------
 
+(defun spy:secret/init ()
+  "Load secret's root init.el."
+  (if (spy:secret/has)
+      (sss:secret/load.root "init")
+
+    ;; No secrets for this system.
+    (let ((path/init (spy:path/to-file (sss:secret/path/system) "init.el")))
+      (mis0/init/warning "spy:secret/init"
+                         (mapconcat #'identity
+                                    '("%s: Cannot init; system has no secrets."
+                                      "  hash: %s"
+                                      "  ID:   %s"
+                                      "  init: %s"
+                                      "   - exists?: %s")
+                                    "\n")
+                         "[SKIP]"
+                         (spy:secret/hash)
+                         (spy:secret/id)
+                         path/init
+                         (if (file-exists-p path/init)
+                             "yes"
+                           "no")))))
+;; (spy:secret/init)
+
+
+;;------------------------------------------------------------------------------
+;; Configuration
+;;------------------------------------------------------------------------------
+
+(defun spy:secret/config ()
+  "Configure this system's secrets."
+  (if (spy:secret/has)
+      (sss:secret/load.root "config")
+
+    ;; No secrets for this system.
+    (let ((path/init (spy:path/to-file (sss:secret/path/system) "init.el"))
+          (path/config (spy:path/to-file (sss:secret/path/system) "config.el")))
+      (mis0/init/warning "spy:secret/init"
+                         (mapconcat #'identity
+                                    '("%s: Cannot init; system has no secrets."
+                                      "  hash:   %s"
+                                      "  ID:     %s"
+                                      "  init:   %s"
+                                      "   - exists?: %s"
+                                      "  config: %s"
+                                      "   - exists?: %s")
+                                    "\n")
+                         "[SKIP]"
+                         (spy:secret/hash)
+                         (spy:secret/id)
+                         path/init
+                         (if (file-exists-p path/init)
+                             "yes"
+                           "no")
+                         path/config
+                         (if (file-exists-p path/config)
+                             "yes"
+                           "no")))))
+;; (spy:secret/config)
 
 
 ;;------------------------------------------------------------------------------
