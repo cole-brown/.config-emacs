be4b3fec451e9eb0d4f57f8599edd088fececa6e
Cole Brown <git@spydez.com>
2021-07-20 12:55:20 -0700

    Attempted fix for server-start.
    If 'runemacs' is run, my config would always try to start the server...
      - So apparently `server-started-p' was insufficient.
    
    Renamed `server-started-p' to `spy:emacs/server:process?'.
    
    Added a func for getting file path and a func for checking if that file
    exists.
    
    Combined the two predicates in new `spy:emacs/server:running?' function.
      - Use this to check/start server.
      - Added mis/init/warning output for when server is running.

--- legacy/sn003-doom/config/daemons.el
+++ legacy/sn003-doom/config/daemons.el
@@ -13,18 +13,71 @@
 (require 'server)
 
 
+;; server-auth-dir
+
+
+;; server-socket-dir
+
+
+;; server-name
+;;   The command server-start makes use of this.  It should not be
+;;   changed while a server is running.
+;;   If this is a file name with no leading directories, Emacs will
+;;   create a socket file by that name under server-socket-dir
+;;   if server-use-tcp is nil, else under server-auth-dir.
+;;   If this is an absolute file name, it specifies where the socket
+;;   file will be created.  To have emacsclient connect to the same
+;;   socket, use the "-s" switch for local non-TCP sockets, and
+;;   the "-f" switch otherwise.
+
+
+(defun spy:emacs/server:path ()
+  "Path to server file."
+  (spy:path/join (if (and server-use-tcp
+                          server-socket-dir)
+                     server-socket-dir
+                   server-auth-dir)
+                 server-name))
+
+
+(defun spy:emacs/server:file? ()
+  "Returns non-nil if server file exists."
+  ;; Windows 10: `server-use-tcp' is `t' (`server-socket-dir' is `nil') and...
+  ;; server file gets put into `server-auth-dir'.
+
+  ;; Return the path? Or just t/nil from `file-exists-p'?
+  (file-exists-p (spy:emacs/server:path)))
+;; (spy:emacs/server:file.exists)
+
+
 ;; `server-running-p' doesn't work.
 ;;   - http://emacshorrors.com/posts/determining-if-the-server-is-started-or-the-wonders-of-server-running-p.html
 ;;     -> http://lists.gnu.org/archive/html/bug-gnu-emacs/2018-06/msg00723.html
 ;; (unless (server-running-p) (server-start))
-(defun server-started-p ()
-    "Return non-nil if this Emacs has a server started."
+(defun spy:emacs/server:process? ()
+    "Return non-nil if this Emacs has a server process."
     (bound-and-true-p server-process))
+;; (spy:emacs/server:process?)
 
 
-(unless (server-started-p)
-  (mis0/init/message "Starting server.")
+(defun spy:emacs/server:running? ()
+    "Return non-nil if this Emacs has a server process and a server file."
+    (and (spy:emacs/server:process?)
+         (spy:emacs/server:file?)))
+;; (spy:emacs/server:running?)
+
+
+(if (spy:emacs/server:running?)
+    ;; Ignore - already running.
+    (mis0/init/warning "spy:emacs/server:running?"
+                       "[daemons] Server already running: %s"
+                       (spy:emacs/server:path))
+
+  ;; Start up a server.
+  (mis0/init/message "[daemons] Starting server.")
   (server-start))
+
+
 
 
 ;;------------------------------------------------------------------------------
