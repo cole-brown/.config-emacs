5084fa4abf1a72bc837b9ea38e90073d0c7b7013
Cole Brown <git@spydez.com>
2021-07-21 09:11:18 -0700

    Adjusted `org-headline-done' face.
    `org-headline-done':
      - Put the two (!) `org-headline-done' setting together so they're more
        obvious.
      - Commented out line setting `org-headline-done' to darker `green-2'.
      - Changed the gray (`base7') to lighten by 0.3 for better visibility.
    
    Changed `(... ,(...)) lists in `custom-theme-set-faces!' to be
    (list ... (...)).
    
    Did an indent of the whole file too to fix indentation.

--- legacy/sn003-doom/config/theme/zenburn.el
+++ legacy/sn003-doom/config/theme/zenburn.el
@@ -19,95 +19,98 @@
                )))
 
     (face-spec-set 'spy:theme.face/org.todo.keyword/todo
-      (list (cons
-             ;; display type
-             t
-             ;; attributes
-             face-plist)))
+                   (list (cons
+                          ;; display type
+                          t
+                          ;; attributes
+                          face-plist)))
 
     (face-spec-set 'spy:theme.face/org.todo.keyword/background
-      (list (cons
-             ;; display type
-             t
-             ;; attributes
-             (list :background (doom-color 'bg-alt)))))
+                   (list (cons
+                          ;; display type
+                          t
+                          ;; attributes
+                          (list :background (doom-color 'bg-alt)))))
 
     (face-spec-set 'spy:theme.face/org.todo.keyword/done.good
-      (list (cons
-             ;; display type
-             t
-             ;; attributes
-             (list :background (doom-color 'bg-alt)
-                   :inherit 'org-done))))
+                   (list (cons
+                          ;; display type
+                          t
+                          ;; attributes
+                          (list :background (doom-color 'bg-alt)
+                                :inherit 'org-done))))
 
     (face-spec-set 'spy:theme.face/org.todo.keyword/done.bad
-      (list (cons
-             ;; display type
-             t
-             ;; attributes
-             (list :background (doom-color 'bg-alt)
-                   :foreground (doom-darken (doom-color 'red-4) 0.1)))))
+                   (list (cons
+                          ;; display type
+                          t
+                          ;; attributes
+                          (list :background (doom-color 'bg-alt)
+                                :foreground (doom-darken (doom-color 'red-4) 0.1)))))
 
     (face-spec-set 'spy:theme.face/org.todo.keyword/info
-      (list (cons
-             ;; display type
-             t
-             ;; attributes
-             (list :background (doom-color 'bg-alt)
-                   :foreground (doom-color 'base7)))))
+                   (list (cons
+                          ;; display type
+                          t
+                          ;; attributes
+                          (list :background (doom-color 'bg-alt)
+                                :foreground (doom-color 'base7)))))
 
     (face-spec-set 'spy:theme.face/org.todo.keyword/null
-      (list (cons
-             ;; display type
-             t
-             ;; attributes
-             (list :background (doom-color 'bg-alt)
-                   :foreground (doom-color 'base7))))))
+                   (list (cons
+                          ;; display type
+                          t
+                          ;; attributes
+                          (list :background (doom-color 'bg-alt)
+                                :foreground (doom-color 'base7))))))
 
 
   (custom-theme-set-faces! 'doom-zenburn
     ;;---
     ;; Done states - little less dark.
     ;;---
-    `(org-done :foreground ,(doom-darken (doom-color 'green-2) 0.15))
-    `(org-agenda-done :foreground ,(doom-color 'base7))
-    `(org-headline-done :foreground ,(doom-color 'base7))
-    `(org-checkbox-statistics-done :foreground ,(doom-darken (doom-color 'green-2) 0.15))
+    (list 'org-done :foreground (doom-darken (doom-color 'green-2) 0.15))
+    (list 'org-agenda-done :foreground (doom-color 'base7))
+    (list 'org-checkbox-statistics-done :foreground (doom-darken (doom-color 'green-2) 0.15))
+    ;; "Done" Headlines - success/fail/info/etc.
+    ;;    - Would be nice to have green, red, and gray... but we only have the one 'done' face.
+    ;;    - So a lighter gray?
+    (list 'org-headline-done :foreground (doom-lighten (doom-color 'base7) 0.3))
+    ;; (list 'org-headline-done :foreground (doom-darken (doom-color 'green-2) 0.15))
 
     ;;---
     ;; Org Headlines - adjust a few to be less dark.
     ;;---
     ;; Some unique, headliney colors.
-    `(outline-1 :foreground ,(doom-color 'orange))
-    `(outline-2 :foreground ,(doom-color 'green+2))
-    `(outline-3 :foreground ,(doom-color 'blue-1))
-    `(outline-4 :foreground ,(doom-color 'red-1))
+    (list 'outline-1 :foreground (doom-color 'orange))
+    (list 'outline-2 :foreground (doom-color 'green+2))
+    (list 'outline-3 :foreground (doom-color 'blue-1))
+    (list 'outline-4 :foreground (doom-color 'red-1))
     ;; ...and repeat.
-    `(outline-5 :foreground ,(doom-color 'orange))
-    `(outline-6 :foreground ,(doom-color 'green+2))
-    `(outline-7 :foreground ,(doom-color 'blue-1))
-    `(outline-8 :foreground ,(doom-color 'red-1))
+    (list 'outline-5 :foreground (doom-color 'orange))
+    (list 'outline-6 :foreground (doom-color 'green+2))
+    (list 'outline-7 :foreground (doom-color 'blue-1))
+    (list 'outline-8 :foreground (doom-color 'red-1))
     ;; ...and 9+ starts over at `outline-1'.
 
     ;;---
     ;; Org TODO States
     ;;---
     ;; ├CURRENT┤
-    `(+org-todo-active :foreground ,(doom-color 'violet)
-                       :background ,(doom-color 'bg-alt))
+    (list '+org-todo-active :foreground (doom-color 'violet)
+          :background (doom-color 'bg-alt))
     ;; ├WAITING┤, ├HOLDING┤
-    `(+org-todo-onhold :foreground ,(doom-darken (doom-color 'magenta) 0.3)
-                       :background ,(doom-color 'bg-alt))
+    (list '+org-todo-onhold :foreground (doom-darken (doom-color 'magenta) 0.3)
+          :background (doom-color 'bg-alt))
     ;; ├PROJECT┤
-    `(+org-todo-project :foreground ,(doom-color 'blue-3)
-                        :background ,(doom-color 'bg-alt))
+    (list '+org-todo-project :foreground (doom-color 'blue-3)
+          :background (doom-color 'bg-alt))
 
 
     ;;---
     ;; "#+DOC_KEYWORD" - needs to be slightly lighter.
     ;;---
-    `(org-document-info-keyword :foreground ,(doom-color 'base7))
-    )
+    (list 'org-document-info-keyword :foreground (doom-color 'base7)))
   ;; +org-todo-active  - bold green
   ;; +org-todo-onhold  - orangey
   ;; +org-todo-project - darker green than bold green
@@ -131,27 +134,27 @@
     ;;---
     ;; "Good" Whitespace
     ;;---
-    `(whitespace-space :foreground ,(doom-color 'base6)) ;; zenburn-bg+2
-    `(whitespace-newline :foreground ,(doom-color 'base6)) ;; zenburn-bg+2
+    (list 'whitespace-space :foreground (doom-color 'base6)) ;; zenburn-bg+2
+    (list 'whitespace-newline :foreground (doom-color 'base6)) ;; zenburn-bg+2
     ;; whitespace-hspace - leave as-is?
     ;; whitespace-indentation - leave as-is?
 
     ;;---
     ;; "Ambivalent" Whitespace
     ;;---
-    `(whitespace-tab :foreground ,(doom-color 'orange)) ;; zenburn-orange
+    (list 'whitespace-tab :foreground (doom-color 'orange)) ;; zenburn-orange
     ;; whitespace-empty - what is this?
-    ;; `(whitespace-empty :foreground ,(doom-color 'red))
+    ;; (list 'whitespace-empty :foreground (doom-color 'red))
 
     ;;---
     ;; "Bad" Whitespace
     ;;---
-    `(whitespace-line :foreground ,(doom-color 'magenta)
-                      :background ,(doom-color 'bg-alt)) ;; Soften the bg color.
+    (list 'whitespace-line :foreground (doom-color 'magenta)
+          :background (doom-color 'bg-alt)) ;; Soften the bg color.
     ;; whitespace-trailing - leave as-is?
     ;; whitespace-big-indent - Don't know if I've ever seen this... leave as-is for now.
-    `(whitespace-space-after-tab :foreground ,(doom-color 'bg-alt)
-                                 :background ,(doom-color 'magenta)) ;; less eyebleed
-    `(whitespace-space-before-tab :foreground ,(doom-color 'bg-alt)
-                                  :background ,(doom-color 'violet)) ;; less eyebleed
+    (list 'whitespace-space-after-tab :foreground (doom-color 'bg-alt)
+          :background (doom-color 'magenta)) ;; less eyebleed
+    (list 'whitespace-space-before-tab :foreground (doom-color 'bg-alt)
+          :background (doom-color 'violet)) ;; less eyebleed
     ))
