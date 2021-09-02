68a7361c6f069a53c45e625caefa1e5294dfc6d7
Cole Brown <git@spydez.com>
2021-09-02 14:34:48 -0700

    org-mode: RET<->S-RET, org-journal keybinds.
    Swap RET & S-RET.
      - RET should be a normal return.
      - S-RET can be 'guess the indentation'.
    
    Attempt to make org-journal keybinds... function properly.

--- legacy/sn003-doom/config/keybinds/org-mode.el
+++ legacy/sn003-doom/config/keybinds/org-mode.el
@@ -29,16 +29,40 @@
 ;; ;; https://yiufung.net/post/org-mode-hidden-gems-pt1/
 ;; ("C-c <tab>" . #'org-kill-note-or-show-branches)
 
+;;------------------------------
+;; Org-Mode & the Thousand Different "RET" Key Functions
+;;------------------------------
+
+;; Auto-indent is a bit odd... Try swapping around RET and S-RET these so the literal newline is on normal enter? Also, `evil-org-return'
+;; is... fucked up. It's description says 'Pressing return twice cancels the continuation of the itemlist or table.', but it's not used for
+;; continuing lists. "C-RET" is - `+org/insert-item-below'.
+;; But you can do "C-RET C-u" to delete the indentation...
+;; ...But you can keep doing that to delete previous lines. So - fuck that noise.
+;; And it's attempt to detect that you're on an empty line (eolp) and (bolp) to do a literal `org-return' isn't working for me?
+;; ...Am I using `evil-org-return' even? I don't think so...
+;; +org/shift-return
+
+(after! evil-org
+  (map! :map evil-org-mode-map
+    ;; Fuck your "more intuitive RET keybinds", Doom.
+    :i [return]   #'+org/shift-return
+    :i "RET"      #'+org/shift-return
+    :i [S-return] (cmd! (org-return electric-indent-mode))
+    :i "S-RET"    (cmd! (org-return electric-indent-mode))
+  ))
+
 
 ;;------------------------------------------------------------------------------------------------------------------------------------------
 ;; Org-Agenda
 ;;------------------------------------------------------------------------------------------------------------------------------------------
 
-;;---
-;; Thanks to: https://emacs.stackexchange.com/a/20438
-;;---
-
 (after! org-mode
+  ;;------------------------------
+  ;; Org-Agenda & the Billion Orphaned Org Buffers
+  ;;------------------------------
+  ;; Thanks to: https://emacs.stackexchange.com/a/20438
+  ;;---
+
   ;; `org-agenda-quit' is evil - it leaves a billion org mode buffers open.
   ;; And it's innocently sat there at "q" that apparently nothing in evil overrides...
   (bind-key [remap org-agenda-quit]
@@ -56,9 +80,8 @@
 ;; Org-Journal
 ;;------------------------------------------------------------------------------------------------------------------------------------------
 
-(spy:secret/if "config/keybinds/org-mode.el"
-    '(:skip "Skipping org-journal keybind config."
-      :eval "")
+(defun sss:keybinds/org-journal ()
+  "Run the org-journal keybinds."
   (when (jerky/namespace/has :work)
     ;; Add `:home' namespaced org-journal stuff.
     (when (featurep! :lang org +journal)
@@ -147,6 +170,28 @@
                                                         (sss:org.journal/namespaced
                                                          :home
                                                          (funcall-interactively #'org-journal-search-forever nil))))))))))
+
+(spy:secret/if "config/keybinds/org-mode.el"
+    '(:skip "Skipping org-journal keybind config."
+      :eval "")
+  ;; Issue:
+  ;;   Getting: `invalid string 'nil'`
+  ;;
+  ;; Re-evaluating the `map!' here fixed it.
+  ;;   - Thought it was a one off. See [[*"SPC n j ww" errors out?][here]].
+  ;;     + ...but it's a problem every start.
+  ;;
+  ;; See:
+  ;;   file:~/.lily.d/taskspace/work/2021-02-05_1_emacs-27-on-windows.notes.org
+  ;;   "SPC n j ww" still a problem
+
+  ;; Set up the org-journal keybinds - want them to exist before org-journal is auto-loaded.
+  (sss:keybinds/org-journal)
+
+  ;; Also set up the keybinds after `org-journal' loaded?
+  ;; TODO: Figure out the not-just-a-bandaid solution?
+  (after! org-journal
+    (sss:keybinds/org-journal)))
 
 
 ;;------------------------------------------------------------------------------------------------------------------------------------------
