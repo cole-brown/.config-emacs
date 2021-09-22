b019011cd487466250347d7d14288afdde9fe62f
Cole Brown <git@spydez.com>
2021-09-22 10:27:35 -0700

    Added ':spy/buffer/+eval' module.
    Added 'eval buffer' function that forces re-eval of defvars/defconsts.
    
    Reordered loads in 'init.el' to be alphabetical.

--- legacy/sn003-doom/modules/spy/buffer/init.el
+++ legacy/sn003-doom/modules/spy/buffer/init.el
@@ -6,18 +6,20 @@
 ;;------------------------------------------------------------------------------
 
 ;; Always load unless specifically removed.
-(unless (featurep! -point)
-   (load! "+point"))
-(unless (featurep! -line)
-   (load! "+line"))
-(unless (featurep! -search)
-   (load! "+search"))
-(unless (featurep! -name)
-   (load! "+name"))
 (unless (featurep! -delete)
    (load! "+delete"))
+(unless (featurep! -point)
+   (load! "+eval"))
+(unless (featurep! -line)
+   (load! "+line"))
 (unless (featurep! -manage)
    (load! "+manage"))
+(unless (featurep! -name)
+   (load! "+name"))
+(unless (featurep! -point)
+   (load! "+point"))
+(unless (featurep! -search)
+   (load! "+search"))
 
 
 ;;------------------------------------------------------------------------------
