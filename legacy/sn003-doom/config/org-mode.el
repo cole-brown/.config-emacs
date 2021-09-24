1b1d8c3d8280d001dfdfcb3748855787313de744
Cole Brown <git@spydez.com>
2021-09-24 12:03:33 -0700

    Updated to remove riskiness from `org-journal-dir'

--- legacy/sn003-doom/config/org-mode.el
+++ legacy/sn003-doom/config/org-mode.el
@@ -428,7 +428,8 @@
                           (jerky/get 'path 'org 'journal
                                      :namespace (jerky/get 'namespace 'system)))
   (unless (dlv:var:safe/predicate? 'org-journal-dir)
-    (dlv:var:safe.predicate 'org-journal-dir #'file-directory-p))
+    ;; It's marked as risky - force it to safe?
+    (dlv:var:safe.predicate 'org-journal-dir #'file-directory-p :quiet))
 
   ;; Tack day name onto our format for the org-journal headline.
   (customize-set-variable 'org-journal-date-format
