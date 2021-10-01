4d11068dab229b7221180c5bb95771e51ac7d13f
Cole Brown <git@spydez.com>
2021-10-01 10:17:29 -0700

    Missed this filed in search commit.
    Moved search keybinds out of 'keybinds/spy-leader.el' and into
    'keybinds/search.el'.

--- legacy/sn003-doom/config/keybinds/spy-leader.el
+++ legacy/sn003-doom/config/keybinds/spy-leader.el
@@ -177,29 +177,6 @@ If no region is active, fills current line."
 
 
 ;;------------------------------------------------------------------------------
-;; Search
-;;------------------------------------------------------------------------------
-
-(defun spy:cmd:search/project/dir (directory &optional arg)
-  "Conduct a text search in the supplied directory.
-If prefix ARG is set, include ignored/hidden files."
-  (interactive (list (read-directory-name "Start Search: ")
-                     current-prefix-arg))
-  (let* ((projectile-project-root nil)
-         (disabled-command-function nil))
-    (funcall-interactively #'+ivy/project-search arg nil directory)))
-
-
-(defun spy:cmd:search/project/here (&optional arg)
-  "Conduct a text search in the current buffer's `default-directory'.
-If prefix ARG is set, include ignored/hidden files."
-  (interactive "P")
-  (funcall-interactively #'spy:cmd:search/project/dir
-                         default-directory
-                         arg))
-
-
-;;------------------------------------------------------------------------------
 ;; Evil Spy Stuff.
 ;;------------------------------------------------------------------------------
 ;; See Doom's keybindings for how to do complicated things:
@@ -211,13 +188,6 @@ If prefix ARG is set, include ignored/hidden files."
       ;;------------------------------
       (:prefix ("-" . "spy") ; Not my first choice but no one uses dash,
                                         ; and it's easy on Dvorak.
-
-       ;;------------------------------
-       ;; Search
-       ;;------------------------------
-       (:prefix ("/" . "Search")
-        :desc "Search from here..."       "/" #'spy:cmd:search/project/here
-        :desc "Search from directory..."  "s" #'spy:cmd:search/project/dir)
 
        ;;------------------------------
        ;; Alignment
