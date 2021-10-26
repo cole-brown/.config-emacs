5f5c969b8aa510b8d46b8b6ab4666ff419d94746
Cole Brown <git@spydez.com>
2021-10-26 09:52:02 -0700

    ':input/keyword': test utils.el
    Added 'test/utils.el' for testing 'utils.el' functions.
    
    Changed `error' calls in 'utils.el' to use `int<keyboard>:output'
    instead.
    
    Changed `int<keyboard>:normalize->string' to use
    `replace-regexp-in-string' instead of a bunch of `string-remove-prefix'
    so that it was better at its job and easier to see/explain what it's
    doing.
    
    Changed `int<keyboard>:normalize->keyword' to use
    `int<keyboard>:normalize->string' for all except `nil' input so that
    keywords would be normalized instead of left as-is.

--- legacy/sn003-doom/modules/input/keyboard/utils.el
+++ legacy/sn003-doom/modules/input/keyboard/utils.el
@@ -8,6 +8,8 @@
 ;;             Not everyone uses Qwerty - there are dozens of us!             ;;
 ;;                                 ──────────                                 ;;
 
+(imp:require :input 'keyboard 'output)
+
 
 ;;------------------------------------------------------------------------------
 ;; Utility Functions & Such That Don't Really Fit Elsewhere
@@ -31,18 +33,22 @@ E.g.
   1) \"dvorak\" -> \"dvorak\"
   2) `:dvorak' -> \"dvorak\"
   3) `+layout/dvorak' -> \"dvorak\""
-  ;; Remove keyword's leading ":"?
-  (string-remove-prefix
-   ":"
-   ;; Remove the rest of module flag's leading "+layout/"?
-   (string-remove-prefix
-    "layout/"
-    ;; Remove leading "+" from layout dir name or module flag.
-    (string-remove-prefix
-     "+"
-     (if (stringp input)
-         input
-       (symbol-name input))))))
+  (replace-regexp-in-string
+   ;; Must match start of string.
+   (rx string-start
+       ;; ':' from keywords (`:dvorak').
+       ;; '+' from module flag (`+layout/dvorak').
+       (zero-or-one (any ":" "+"))
+       ;; 'layout/' from module flag (`+layout/dvorak') or directory path ('layout/+dvorak').
+       (zero-or-one "layout/")
+       ;; '+' from directory path ('layout/+dvorak').
+       (zero-or-one "+"))
+   ;; Replace with nothing.
+   ""
+   ;; Make sure we've got a string (or we've errored out on invalid input type.
+   (if (stringp input)
+       input
+     (symbol-name input))))
 ;; (int<keyboard>:normalize->string '+layout/spydez)
 ;; (int<keyboard>:normalize->string :spydez)
 ;; (int<keyboard>:normalize->string "spydez")
@@ -52,20 +58,16 @@ E.g.
 (defun int<keyboard>:normalize->keyword (input)
   "Convert INPUT to a keyboard layout keyword.
 
-If INPUT is:
-  - Keyword: It is returned as is.
-  - nil:     nil will be returned (allows for default args).
+If INPUT is `nil', `nil' will be returned (allows for default args).
 
 Otherwise INPUT is normalized to a string and then converted to a keyword.
+  - Uses `int<keyboard>:normalize->string'.
 
 E.g. `+layout/dvorak' -> `:dvorak'."
-  (cond ((null input)
-         nil)
-        ((keywordp input)
-         input)
-        (t
-         (intern (concat ":"
-                         (int<keyboard>:normalize->string input))))))
+  (if (null input)
+      nil
+    (intern (concat ":"
+                    (int<keyboard>:normalize->string input)))))
 ;; (int<keyboard>:normalize->keyword '+layout/spydez)
 ;; (int<keyboard>:normalize->keyword :spydez)
 ;; (int<keyboard>:normalize->keyword "spydez")
@@ -88,13 +90,18 @@ For example, (list 'normal 'visual 'insert) will map to `:nvi'. See
     (dolist (state states)
       (if-let ((state/char (nth 0 (rassoc (doom-unquote state) doom-evil-state-alist))))
           (push state/char keyword/char-list)
-        (error "int<keyboard>:states->keyword: Invalid state: %S" state)))
+        (int<keyboard>:output :error
+                              "int<keyboard>:states->keyword"
+                              "Invalid state: %S"
+                              state)))
     ;; And now convert our list of chars into a keyword.
     (if keyword/char-list
         (intern (apply #'string ?: (nreverse keyword/char-list)))
-      (error (concat "int<keyboard>:states->keyword: No result from states? "
-                     "states: %S -> keyword characters: %S")
-             states keyword/char-list))))
+      (int<keyboard>:output :error
+                            "int<keyboard>:states->keyword"
+                            '("No result from states? "
+                              "states: %S -> keyword characters: %S")
+                            states keyword/char-list))))
 ;; (int<keyboard>:states->keyword '(normal visual))
 
 
