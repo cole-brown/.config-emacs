388e78a89a913906e8e6024629b9b6830fd02db4
Cole Brown <code@brown.dev>
2021-11-05 08:57:46 -0700

    unit tests

--- legacy/sn003-doom/modules/input/keyboard/test/layout/types/define.el
+++ legacy/sn003-doom/modules/input/keyboard/test/layout/types/define.el
@@ -152,69 +152,38 @@
     (should-not (int<keyboard>:layout/types:valid/keyword? t :layout:evil:test))
     (should-not (int<keyboard>:layout/types:valid/keyword? nil :layout:common:thing))
     (should-not (int<keyboard>:layout/types:valid/keyword? ':evil :layout:common:thing))
+    (should-not (int<keyboard>:layout/types:valid/keyword? var/invalid/type :layout:common:thing))
 
     ;;------------------------------
     ;; 'Not a (Valid) Keyword' == invaild
     ;;------------------------------
-
     (should-not (int<keyboard>:layout/types:valid/keyword? :evil t))
     (should-not (int<keyboard>:layout/types:valid/keyword? :evil nil))
     (should-not (int<keyboard>:layout/types:valid/keyword? :evil ':quoted-keyword/invalid))
-    (should-not (int<keyboard>:layout/types:valid/keyword? :evil ':layout:evil:quoted-keyword/valid))
-    (let ((var/invalid/keyword :layout:invalid-keyword)
-          (var/valid/type :common))
-
-
+    (should-not (int<keyboard>:layout/types:valid/keyword? :evil var/invalid/keyword))
 
     ;;------------------------------
-    ;; Is our keyword validator valid?
+    ;; Valid Keyword, but not for the Type.
     ;;------------------------------
+    (should-not(int<keyboard>:layout/types:valid/keyword? :evil :layout:emacs:keyword/valid))
+    (should-not(int<keyboard>:layout/types:valid/keyword? :evil ':layout:emacs:quoted-keyword/valid))
 
-
-    (let ((func #'ignore))
-      (should (eq func
-                  (int<keyboard>:layout/types:valid/keyword? func))))
-
-    (let ((func #'does-not-exist-yet))
-      (should (eq func
-                  (int<keyboard>:layout/types:valid/keyword? func))))
-
+    (should-not (int<keyboard>:layout/types:valid/keyword? var/valid/type   var/valid/keyword-2))
+    (should-not (int<keyboard>:layout/types:valid/keyword? var/valid/type-2 var/valid/keyword))
 
     ;;------------------------------
-    ;; Keyword -> Function
+    ;; Valid!
     ;;------------------------------
+    (should (int<keyboard>:layout/types:valid/keyword? :evil :layout:evil:keyword/valid))
+    (should (int<keyboard>:layout/types:valid/keyword? :evil ':layout:evil:quoted-keyword/valid))
 
-    ;; Create some keyword->func pairs...
-    ;; Some for real functions, some for funcs that don't exist (yet).
-    (setq int<keyboard>:layout/types:keywords '((:dne-0    . #'does-not-exist-yet)
-                                                (:dne-1    . #'hello-there)
-                                                (:exists-0 . #'ignore)
-                                                (:exists-1 . #'identity)))
-    ;; Make sure the alist is what we're expecting.
-    (let ((keyword/assoc (assoc :dne-0 int<keyboard>:layout/types:keywords)))
-      (should (eq :dne-0
-                  (nth 0 keyword/assoc)))
-      (should (eq 'function
-                  (nth 1 keyword/assoc)))
-      (should (eq 'does-not-exist-yet
-                  (nth 2 keyword/assoc))))
+    (should (int<keyboard>:layout/types:valid/keyword? :emacs :layout:emacs:keyword/valid))
+    (should (int<keyboard>:layout/types:valid/keyword? :emacs ':layout:emacs:quoted-keyword/valid))
 
-    ;; Make sure our not-functions and functions are as they should be.
-    (should-error (funcall (nth 1 (alist-get :dne-0 int<keyboard>:layout/types:keywords))))
-    (should-error (funcall (nth 1 (alist-get :dne-1 int<keyboard>:layout/types:keywords))))
-    ;; Make sure our functions don't error or anything eggregious.
-    (should-not (funcall (nth 1 (alist-get :exists-0 int<keyboard>:layout/types:keywords))))
-    (should (eq :multipass
-                (funcall (nth 1 (alist-get :exists-1 int<keyboard>:layout/types:keywords)) :multipass)))
+    (should (int<keyboard>:layout/types:valid/keyword? :common :layout:common:keyword/valid))
+    (should (int<keyboard>:layout/types:valid/keyword? :common ':layout:common:quoted-keyword/valid))
 
-    (should (equal '(function does-not-exist-yet)
-                   (int<keyboard>:layout/types:valid/keyword? :dne-0)))
-    (should (equal '(function hello-there)
-                   (int<keyboard>:layout/types:valid/keyword? :dne-1)))
-    (should (equal '(function ignore)
-                   (int<keyboard>:layout/types:valid/keyword? :exists-0)))
-    (should (equal '(function identity)
-                   (int<keyboard>:layout/types:valid/keyword? :exists-1)))
+    (should (int<keyboard>:layout/types:valid/keyword? var/valid/type   var/valid/keyword))
+    (should (int<keyboard>:layout/types:valid/keyword? var/valid/type-2 var/valid/keyword-2))
 
-    ;; Make sure that trying to normalize an unknown keyword is an error.
-    (should-error (int<keyboard>:layout/types:valid/keyword? :sir-not-appearing-in-this-alist))))
+    (should (int<keyboard>:layout/types:valid/keyword? :evil ':layout:evil:quoted-keyword/valid)))))
