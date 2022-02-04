2483873fe40fdf90de59e34767f287ffbb720aa9
Cole Brown <code@brown.dev>
2022-02-04 09:49:49 -0800

    Config for hydras themselves.
    None currently, but a good amount of notes while I was making the file,
    so I'm saving it.

---
+++ legacy/sn003-doom/config/hydra.el
@@ -0,0 +1,77 @@
+;;; config/hydra.el -*- lexical-binding: t; -*-
+
+
+;; ┌──────────────────────────────────═══───────────────────────────────────┐ ;;
+;; │                                 ═════                                  │ ;;
+;; │           Multi-Headed Monster - Major, Pretty, and Original           │ ;;
+;; │                                 ═════                                  │ ;;
+;; └──────────────────────────────────═══───────────────────────────────────┘ ;;
+
+
+;;------------------------------------------------------------------------------
+;; Major-Mode & Pretty Hydra
+;;------------------------------------------------------------------------------
+;; NOTE: Major-Mode Hydra includes Pretty-Hydra, but you can install
+;; Pretty-Hydra on its own if you want...
+;;   https://github.com/jerrypnz/major-mode-hydra.el
+;;   https://github.com/jerrypnz/major-mode-hydra.el#pretty-hydra
+
+;; NOTE: Slow Rendering
+;; If you experience a slow down in performance when rendering multiple icons
+;; simultaneously, you can try setting the following (emacs) variable:
+;;   (setq inhibit-compacting-font-caches t)
+
+;; NOTE: Troubleshooting
+;;   TODO: Some of my Font Awesome icons are wrong, so I might have to do this...
+;; https://github.com/domtronn/all-the-icons.el#troubleshooting
+
+;;
+;;
+;; No package setup, currently.
+;;
+;;
+
+
+;;------------------------------------------------------------------------------
+;; Major-Mode Hydra
+;;------------------------------------------------------------------------------
+;; https://github.com/jerrypnz/major-mode-hydra.el#major-mode-hydra
+
+;;
+;;
+;; No package setup, currently.
+;;
+;;
+
+
+;;------------------------------------------------------------------------------
+;; Pretty Hydra
+;;------------------------------------------------------------------------------
+;; https://github.com/jerrypnz/major-mode-hydra.el#pretty-hydra
+
+;; `with-faicon'
+;;   - Uses `all-the-icons-faicon'.
+;;   - aka "With Font Awesome icon"
+;;
+;; All-the-Icons: Inserting Icons Directly
+;;   https://github.com/domtronn/all-the-icons.el#inserting-icons-directly
+;;
+;; The icon alists are prefixed with: `all-the-icons-data/'
+;;   (pp all-the-icons-data/fa-icon-alist)
+
+;;
+;;
+;; No package setup, currently.
+;;
+;;
+
+
+;;------------------------------------------------------------------------------
+;; Original Hydra
+;;------------------------------------------------------------------------------
+
+;;
+;;
+;; No package setup, currently.
+;;
+;;
