6c886cf4b90e11e39806772a526ae1f4b5375125
Cole Brown <code@brown.dev>
2022-03-15 10:25:06 -0700

    Remove `timestamp` from kill buffer funcs.

--- legacy/sn003-doom/config/work.el
+++ legacy/sn003-doom/config/work.el
@@ -17,11 +17,11 @@
 ;;------------------------------------------------------------------------------
 
 
-(defun spy:cmd:buffer:kill/magit (&optional timestamp)
+(defun spy:cmd:buffer:kill/magit ()
   "Delete all magit buffers."
   (interactive)
   (message "[%s] Kill 'magit' buffers..."
-           timestamp)
+           (spy:datetime/string.get 'iso-8601 'long))
   ;; Magit buffers shouldn't stick around. They get in the way of saving
   ;; workspaces and can easily be reopened.
   (spy:buffer/kill.matching ".*magit.*"
@@ -30,11 +30,11 @@
                             :process))
 
 
-(defun spy:cmd:buffer:kill/deadgrep (&optional timestamp)
+(defun spy:cmd:buffer:kill/deadgrep ()
   "Delete all deadgrep buffers."
   (interactive)
   (message "[%s] Kill 'deadgrep' buffers..."
-           timestamp)
+           (spy:datetime/string.get 'iso-8601 'long))
   (spy:buffer/kill.matching ".*deadgrep.*"
                             :internal
                             :modified
