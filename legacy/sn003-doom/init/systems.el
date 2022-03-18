f9c7592fd88ef5e8466b07d9d60b2d590d7da7b1
Cole Brown <code@brown.dev>
2022-03-18 11:54:13 -0700

    Add Win10 VM to known systems.

--- legacy/sn003-doom/init/systems.el
+++ legacy/sn003-doom/init/systems.el
@@ -26,11 +26,11 @@
 ;;------------------------------------------------------------------------------
 
 
-;;--------------------
+;;------------------------------
 ;; home/2017/desk
 ;;   Host OS:  Windows 10
 ;;   Guests:   Misc Linux via WSL2
-;;--------------------
+;;------------------------------
 
 ;;---
 ;; home/2017/desk::ab48e5-886ff
@@ -42,7 +42,6 @@
                    :type "desk"
                    :description "(Windows/WSL) Home desktop PC built in 2017."
                    :path/secret/root "~/.config/spydez/secret")
-
 
 ;;---
 ;; home/2017/desk::02d29a-8bdef1
@@ -57,17 +56,15 @@
                    :path/secret/root "~/.secret.d")
 
 
-;;--------------------
+;;------------------------------
 ;; work/2021/lap
 ;;   Host OS:  Ubuntu 20.04 LTS
 ;;   Guests:   Windows 10
-;;--------------------
-
+;;------------------------------
 
 ;;---
 ;; work/2021/lap::e29fbd-0d21c7
 ;;   via Ubuntu 20.04
-;;   (formerly known as: home/2017/desk::5730ce-91e149)
 ;;---
 (spy:system/define :hash "e29fbd-0d21c7"
                    :domain "work"
@@ -76,7 +73,29 @@
                    :description "Dell XPS 13 laptop"
                    :path/secret/root "~/.config/secret")
 
+;;---
+;; work/2021/vm::d3dbd4-fa7934
+;;   via Ubuntu 20.04
+;;---
+(spy:system/define :hash "d3dbd4-fa7934"
+                   :domain "work"
+                   :date "2021"
+                   :type "vm"
+                   :description "Windows 10 VM"
+                   :path/secret/root (path:canonicalize user-emacs-directory ".." "secret"))
 
+
+;;------------------------------
+;; NOTE: New Systems!
+;;------------------------------
+
+;; Open Emacs. Check "mis:init:messages" or "*Messages*" buffer. You should see something like:
+;;   > [WARNING `spy:secret:init`]
+;;   >     [SKIP]: Cannot init; system has no secrets.
+;;   >
+;;   >
+;;   >
+;;   > [/WARNING `spy:secret:init`]
 
 ;; Generate a new system's UID using this:
 ;; - "home", "work", other domain
@@ -91,8 +110,6 @@
 
 
 ;;----------------------------------------------------------
-;; Domain: Work
+;; The End.
 ;;----------------------------------------------------------
-
-
 (imp:provide :dot-emacs 'init 'systems)
