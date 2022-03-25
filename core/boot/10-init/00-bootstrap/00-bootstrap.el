408e0a4dfe9c4ac616e064d7361d2b28db544c3c
Cole Brown <code@brown.dev>
2022-03-25 10:23:42 -0700

    New placeholders for 'core/boot/10-init'.
    Replace "placeholder.txt" files in "core/boot/10-init" dirs with
    ready-to-be-filled-in elisp files. Used the placeholder sections from
    "init.el" as the initial code content.

---
+++ core/boot/10-init/00-bootstrap/00-bootstrap.el
@@ -0,0 +1,24 @@
+;;; 00-bootstrap.el --- Super early stuff requried for "all" of init. -*- lexical-binding: t; -*-
+;;
+;; Author: Cole Brown <code@brown.dev>
+;; URL:    https://github.com/cole-brown/.config-emacs
+;;
+;; These are not the GNU Emacs droids you're looking for.
+;; We can go about our business.
+;; Move along.
+;;
+;;; Commentary:
+;;; Code:
+
+
+;;------------------------------------------------------------------------------
+;; Bootstrap
+;;------------------------------------------------------------------------------
+
+;; TODO: load imp & other early/required-for-everything packages/modules
+
+
+;;------------------------------------------------------------------------------
+;; The End.
+;;------------------------------------------------------------------------------
+;; TODO: provide imp symbol
---
+++ core/boot/10-init/20-config/00-config.el
@@ -0,0 +1,22 @@
+;;; 00-config.el --- Configure packages & stuff -*- lexical-binding: t; -*-
+;;
+;; Author: Cole Brown <code@brown.dev>
+;; URL:    https://github.com/cole-brown/.config-emacs
+;;
+;; These are not the GNU Emacs droids you're looking for.
+;; We can go about our business.
+;; Move along.
+;;
+;;; Commentary:
+;;; Code:
+
+
+;;------------------------------------------------------------------------------
+;; Configure?
+;;------------------------------------------------------------------------------
+
+
+;;------------------------------------------------------------------------------
+;; The End.
+;;------------------------------------------------------------------------------
+;; TODO: provide imp symbol
