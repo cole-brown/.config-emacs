65c832efb02b294b9cecf988b33dd8f3da365804
Cole Brown <code@brown.dev>
2022-04-06 09:22:58 -0700

    Bootstrap extra-super-required packages.
    Add `use-package` bootstrap.
    Add `no-littering` bootstrap.

--- core/boot/10-init/00-bootstrap.el
+++ core/boot/10-init/00-bootstrap.el
@@ -19,6 +19,25 @@
 
 
 ;;------------------------------
+;; `use-package'
+;;------------------------------
+;; https://github.com/jwiegley/use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+;;---
+;; Use-Package Global Settings:
+;;---
+
+;; Don't force ensure - let packages lazily auto-load as they think they're needed.
+;; Can always override on a per-package basis.
+;; (customize-set-variable 'use-package-always-ensure t)
+
+
+;;------------------------------
 ;; `imp'
 ;;------------------------------
 (load (expand-file-name "imp/init.el" init:path:core/modules))
@@ -29,7 +48,37 @@
 ;;------------------------------
 ;; `no-littering'
 ;;------------------------------
-;; TODO: this
+(use-package no-littering
+  ;;--------------------
+  :custom
+  ;;--------------------
+  ;; Stick with default "etc"/"var" directory names.
+  ;; (no-littering-etc-directory (expand-file-name "config/" user-emacs-directory))
+  ;; (no-littering-var-directory (expand-file-name "data/"   user-emacs-directory))
+
+
+  ;;--------------------
+  :config
+  ;;--------------------
+  ;; Suggested settings: https://github.com/emacscollective/no-littering#suggested-settings
+
+  ;; `recentf' should ignore the files in the `no-littering' dirs.
+  (add-to-list 'recentf-exclude no-littering-etc-directory)
+  (add-to-list 'recentf-exclude no-littering-var-directory)
+
+  ;; Auto-saves should go in the `no-littering' directory.
+  (setq auto-save-file-name-transforms
+        `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+
+  ;; We don't want a "custom.el" at all, but if we did, it should be in the
+  ;; `no-littering' etc dir.
+  ;; (setq custom-file (no-littering-expand-etc-file-name "custom.el"))
+
+  ;; Native Compliation (Emacs 28+):
+  (when (fboundp 'startup-redirect-eln-cache)
+    (startup-redirect-eln-cache
+     (convert-standard-filename
+      (no-littering-expand-var-file-name "eln-cache/")))))
 
 
 ;;------------------------------------------------------------------------------
