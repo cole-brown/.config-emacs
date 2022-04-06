f983886c41972ca2e92cfc019ddd6656e2757825
Cole Brown <code@brown.dev>
2022-04-06 10:47:31 -0700

    Start of early-init's settings.
    Start of early-init's settings/initialization phase.

---
+++ core/boot/00-early/20-init.el
@@ -0,0 +1,24 @@
+;;; 20-init.el --- Early Init -*- lexical-binding: t; -*-
+;;
+;; Author: Cole Brown <code@brown.dev>
+;; URL:    https://github.com/cole-brown/.config-emacs
+;;
+;; These are not the GNU Emacs droids you're looking for.
+;; We can go about our business.
+;; Move along.
+;;
+;;; Commentary:
+;;; Code:
+
+
+;;------------------------------------------------------------------------------
+;; Debug Variables
+;;------------------------------------------------------------------------------
+
+(defconst init:interactive? (not noninteractive)
+  "If non-nil, Emacs is in interactive mode.")
+
+
+;;------------------------------------------------------------------------------
+;; The End
+;;------------------------------------------------------------------------------
