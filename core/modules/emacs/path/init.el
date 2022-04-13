a038a3fd2c0d01f1764b42b58a423b49a299401c
Cole Brown <code@brown.dev>
2022-04-13 11:02:30 -0700

    Revert "path/init.el" back to just `imp`

--- core/modules/emacs/path/init.el
+++ core/modules/emacs/path/init.el
@@ -17,76 +17,31 @@
 
 
 ;;------------------------------------------------------------------------------
-;; Does `imp' exist?
+;; Set imp Root
 ;;------------------------------------------------------------------------------
-;; Let's be complicated...
-;;   - If `imp' already exists, use it for our file loading.
-;;     - We're a regular ol' package/module/whatever.
-;;   - Otherwise use Emacs' `load' function to load our files.
-;;     - We're a super-early-in-init package/module/whatever.
 
-(if (featurep 'imp)
-    ;;--------------------------------------------------------------------------
-    ;; Use `imp'
-    ;;--------------------------------------------------------------------------
-    (progn
-      ;;------------------------------
-      ;; Set up root.
-      ;;------------------------------
-      (imp:path:root :path
-                     (imp:path:join doom-private-dir
-                                    "modules"
-                                    "emacs"
-                                    "path")
-                     "init.el")
+(imp:path:root :path
+               (imp:path:join doom-private-dir
+                              "modules"
+                              "emacs"
+                              "path")
+               "init.el")
 
-      ;;------------------------------
-      ;; Load Files
-      ;;------------------------------
-      (imp:load :feature  '(:path path)
-                :filename "path")
-      (imp:load :feature  '(:path files)
-                :filename "files")
-      (imp:load :feature  '(:path regex)
-                :filename "regex"))
 
-  ;;----------------------------------------------------------------------------
-  ;; No `imp', load files ourself.
-  ;;----------------------------------------------------------------------------
-  ;; This `cond' is basically `path:current:file'.
-  (if-let* ((current-file (cond
-                           ;;------------------------------
-                           ;; Look for a valid "current file" variable.
-                           ;;------------------------------
-                           ((bound-and-true-p byte-compile-current-file))
+;;------------------------------------------------------------------------------
+;; Load Files
+;;------------------------------------------------------------------------------
 
-                           (load-file-name)
-
-                           ((stringp (car-safe current-load-list))
-                            (car current-load-list))
-
-                           (buffer-file-name)
-
-                           ;;------------------------------
-                           ;; Error: Didn't find anything valid.
-                           ;;------------------------------
-                           ((error "path/init.el: Cannot get this file's path"))))
-            (current-dir (directory-file-name (file-name-directory current-file))))
-      (progn
-        ;;------------------------------
-        ;; Load Files
-        ;;------------------------------
-        ;; Load same files in same order as above in `imp' section.
-        (load (expand-file-name "path" current-dir))
-        (load (expand-file-name "files" current-dir))
-        (load (expand-file-name "regex" current-dir)))
-
-    (error "path/init.el: Cannot get this file's directory path")))
+;; Always load.
+(imp:load :feature  '(:path path)
+          :filename "path")
+(imp:load :feature  '(:path files)
+          :filename "files")
+(imp:load :feature  '(:path regex)
+          :filename "regex")
 
 
 ;;------------------------------------------------------------------------------
 ;; The End.
 ;;------------------------------------------------------------------------------
-(if (featurep 'imp)
-    (imp:provide:with-emacs :path)
-  (provide 'path))
+(imp:provide :path)
