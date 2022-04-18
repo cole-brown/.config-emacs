3b9dbf26997e5085262e7752c7dc0457767f8265
Cole Brown <code@brown.dev>
2022-04-18 10:10:44 -0700

    Move `innit:group` definition to early init.

--- core/boot/00-early/00-bootstrap.el
+++ core/boot/00-early/00-bootstrap.el
@@ -1,6 +1,18 @@
 ;;; core/boot/00-early/00-bootstrap.el -*- lexical-binding: t; -*-
 
 ;;------------------------------------------------------------------------------
+;; Custom Variables
+;;------------------------------------------------------------------------------
+;; Create a group for any custom variables that will be created?
+
+(defgroup innit:group nil
+  "An Emacs framework for running similar inits on one or more systems."
+  :prefix "innit:"
+  :group 'tools
+  :link '(url-link "https://github.com/cole-brown/.config-emacs"))
+
+
+;;------------------------------------------------------------------------------
 ;; Bootstrap
 ;;------------------------------------------------------------------------------
 
--- core/boot/10-init/10-init.el
+++ core/boot/10-init/10-init.el
@@ -25,18 +25,6 @@
 
 
 ;;------------------------------------------------------------------------------
-;; Custom Variables
-;;------------------------------------------------------------------------------
-;; Create a group for any custom variables that will be created?
-
-(defgroup innit nil
-  "An Emacs framework for running similar inits on one or more systems."
-  :prefix "innit:"
-  :group 'tools
-  :link '(url-link "https://github.com/cole-brown/.config-emacs"))
-
-
-;;------------------------------------------------------------------------------
 ;; Initialize!
 ;;------------------------------------------------------------------------------
 
