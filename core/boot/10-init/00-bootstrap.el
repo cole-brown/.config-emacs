e03627fc46827187c20e305ce696777b92d06395
Cole Brown <code@brown.dev>
2022-04-29 09:57:14 -0700

    Fix bug in no-littering `use-package`
    Guard the `recentf` variables with `imp:eval:after` so that it doesn't
    error during the `use-package` config.
    
    `no-littering` wants to be ASAP and `recentf` isn't loaded yet.

--- core/boot/10-init/00-bootstrap.el
+++ core/boot/10-init/00-bootstrap.el
@@ -12,6 +12,15 @@
 
 
 ;;------------------------------------------------------------------------------
+;; Load Paths
+;;------------------------------------------------------------------------------
+
+;; TODO: Do I want to add anything to the load-path directly, or just use `imp'
+;; et al?
+;; (add-to-list 'load-path (path:join user-emacs-directory "..."))
+
+
+;;------------------------------------------------------------------------------
 ;; Required Packages & Modules
 ;;------------------------------------------------------------------------------
 ;; Load a few "must exist ASAP" things that we'll use for the rest of Emacs
@@ -45,8 +54,9 @@
   ;; Suggested settings: https://github.com/emacscollective/no-littering#suggested-settings
 
   ;; `recentf' should ignore the files in the `no-littering' dirs.
-  (add-to-list 'recentf-exclude no-littering-etc-directory)
-  (add-to-list 'recentf-exclude no-littering-var-directory)
+  (imp:eval:after recentf
+    (add-to-list 'recentf-exclude no-littering-etc-directory)
+    (add-to-list 'recentf-exclude no-littering-var-directory))
 
   ;; Auto-saves should go in the `no-littering' directory.
   (setq auto-save-file-name-transforms
@@ -54,6 +64,7 @@
 
   ;; TODO: An innit function to set this or not.
   ;; TODO: Or just `(when some-variable ...)'
+  ;; TODO:   - One for the settings.el file to adjust?
   ;; We don't want a "custom.el" at all, but if we did, it should be in the
   ;; `no-littering' etc dir.
   ;; (setq custom-file (no-littering-expand-etc-file-name "custom.el"))
