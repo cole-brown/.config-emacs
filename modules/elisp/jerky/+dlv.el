5fdf0b9c747c205366ace38382f60a02ca4fb867
Cole Brown <code@brown.dev>
2022-05-17 11:41:28 -0700

    Refactor jerky/+dlv.el debugging

--- modules/elisp/jerky/+dlv.el
+++ modules/elisp/jerky/+dlv.el
@@ -28,25 +28,28 @@
 
 (defun jerky:dlv:namespace/set (directory namespace)
   "Set Jerky's local NAMESPACE for the DIRECTORY."
-  (nub:debug:func/start :jerky
-                        "jerky:dlv:namespace/set"
-                        '(:dlv)
-                        (list (cons 'directory . directory)
-                              (cons 'namespace . namespace)))
+  (let ((func.name "jerky:dlv:namespace/set")
+        (func.tags '(:dlv)))
+    (nub:debug:func/start :jerky
+                          func.name
+                          func.tags
+                          (list (cons 'directory . directory)
+                                (cons 'namespace . namespace)))
 
-  (if (not (jerky:namespace:has namespace))
-      (error "jerky:dlv:namespace/set: No known namespace called '%s'" namespace)
+    (if (not (jerky:namespace:has namespace))
+        (error "%s: No known namespace called '%s'"
+               func.name
+               namespace)
 
-    (int<jerky>:debug "int<jerky>:dlv:namespace/set" "  -> `int<jerky>:dlv:namespace/local'")
-    (dlv:set directory
-             nil ;; global mode
-             (list 'int<jerky>:dlv:namespace/local
-                   namespace
-                   :safe)))
+      (dlv:set directory
+               nil ;; global mode
+               (list 'int<jerky>:dlv:namespace/local
+                     namespace
+                     :safe)))
 
-  (nub:debug:func/end :jerky
-                      "jerky:dlv:namespace/set"
-                      '(:dlv)))
+    (nub:debug:func/end :jerky
+                        func.name
+                        func.tags)))
 ;; (jerky:get 'path 'org 'journal :namespace :work)
 ;;   -> "d:/home/main/.org.d/logbook/work/"
 ;; (jerky:dlv:namespace/set "d:/home/main/.org.d/logbook/work/" :work)
