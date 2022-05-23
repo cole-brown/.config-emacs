27ebcabd26ec9e7aec5a423dd9a1cd78f7600007
Cole Brown <code@brown.dev>
2022-05-23 10:40:55 -0700

    Refactor "dlv/+display.el"
    Update header.
    
    Update docstrings.
    
    Update function names to latest preferences.
    
    etc.

--- core/modules/emacs/dlv/+display.el
+++ core/modules/emacs/dlv/+display.el
@@ -1,4 +1,12 @@
-;;; emacs/dlv/display.el -*- lexical-binding: t; -*-
+;;; dlv/+display.el --- Display DLV info. -*- lexical-binding: t; -*-
+;;
+;;; Commentary:
+;;
+;; Display info for Directory Local Variables.
+;;
+;; Write info to '*Messages*' buffer.
+;;
+;;; Code:
 
 
 (imp:require :dlv 'dlv)
@@ -9,9 +17,15 @@
 ;;------------------------------------------------------------------------------
 
 (defun dlv:check (filepath symbol expected)
-  "Get SYMBOL's value at FILEPATH in a few different ways."
+  "Get SYMBOL's value at FILEPATH in a few different ways.
 
-  (let ((file/existed? (file-exists-p filepath))
+EXPECTED is output with actual value, and also used to check DLV safeness via
+`safe-local-variable-p'.
+
+Output to '*Messages*' buffer."
+  (let ((func/name "dlv:check")
+        ;; (func/tags '(:info))
+        (file/existed? (file-exists-p filepath))
         (buffer/existed? (get-file-buffer filepath))
         buffer/local
         value/ffns
@@ -42,7 +56,7 @@
       (setq buffer/local nil))
 
     ;; Output results.
-    (message (concat "dlv:check:\n"
+    (message (concat "%s:\n"
                      "  inputs:\n"
                      "    filepath:                              %s\n"
                      "    symbol: - - - - - - - - - - - - - - -  %S\n"
@@ -55,6 +69,7 @@
                      "    `enable-local-variables': - - - - - -  %S\n"
                      "    symbol property `safe-local-variable': %S\n"
                      "    assoc `safe-local-variable-values':    %S")
+             func/name
              ;; inputs
              filepath
              symbol
@@ -81,57 +96,70 @@
 ;; Show Local Variable Lists
 ;;------------------------------------------------------------------------------
 
-(defun dlv:buffer-locals/show-all ()
+(defun dlv:cmd:buffer-locals:show/all ()
   "Show all the buffer-local variables and values for the buffer.
 
-NOTE: Huge alist!"
+NOTE: Probably a huge alist!
+
+Output to '*Messages*' buffer."
   (interactive)
-  (int<dlv>:message:boxed.xml :start "dlv:buffer-locals/show-all"
-                              (cons "buffer" (buffer-name))
-                              (cons "path" (buffer-file-name)))
-  (message "")
+  (let ((func/name "dlv:cmd:buffer-locals:show/all")
+        ;; (func/tags '(:cmd :info))
+        )
+    (int<dlv>:message:boxed.xml :start func/name
+                                (cons "buffer" (buffer-name))
+                                (cons "path" (buffer-file-name)))
+    (message "")
 
-  (pp (buffer-local-variables))
-
-  (message "")
-  (int<dlv>:message:boxed.xml :end "dlv:buffer-locals/show-all"))
-
-
-(defun dlv:buffer-locals/show-dlvs ()
-  "Show the dir-local (and file-local) variables and values from the
-buffer-local variable alist.
-
-A very much reduced list from `dlv:buffer-locals/show-all'."
-  (interactive)
-  (int<dlv>:message:boxed.xml :start "dlv:buffer-locals/show-dlvs"
-                              (cons "buffer" (buffer-name))
-                              (cons "path" (buffer-file-name)))
-  (message "")
-
-  (let* ((all-locals (buffer-local-variables))
-         (dir-locals (alist-get 'dir-local-variables-alist
-                                all-locals))
-         (file-locals (alist-get 'file-local-variables-alist
-                                 all-locals)))
-
-    (int<dlv>:message:line ?─)
-    (message "dir-local-variables-alist:")
-    (pp dir-locals)
+    (pp (buffer-local-variables))
 
     (message "")
-    (int<dlv>:message:line ?─)
-    (message "file-local-variables-alist:")
-    (pp file-locals))
-
-  (message "")
-  (int<dlv>:message:boxed.xml :end "dlv:buffer-locals/show-all"))
+    (int<dlv>:message:boxed.xml :end func/name)))
 
 
-(defun dlv:dir-locals/show-all (filepath)
-  "Show all DLVs for filepath."
+(defun dlv:cmd:buffer-locals:show/dlvs ()
+  "Show dir-locals(/file-locals) from the buffer-locals alist.
+
+A very much reduced list from `dlv:buffer-locals:show/all'.
+
+Output to '*Messages*' buffer."
+  (interactive)
+  (let ((func/name "dlv:cmd:buffer-locals:show/dlvs")
+        ;; (func/tags '(:cmd :info))
+        )
+    (int<dlv>:message:boxed.xml :start func/name
+                                (cons "buffer" (buffer-name))
+                                (cons "path" (buffer-file-name)))
+    (message "")
+
+    (let* ((all-locals (buffer-local-variables))
+           (dir-locals (alist-get 'dir-local-variables-alist
+                                  all-locals))
+           (file-locals (alist-get 'file-local-variables-alist
+                                   all-locals)))
+
+      (int<dlv>:message:line ?─)
+      (message "dir-local-variables-alist:")
+      (pp dir-locals)
+
+      (message "")
+      (int<dlv>:message:line ?─)
+      (message "file-local-variables-alist:")
+      (pp file-locals))
+
+    (message "")
+    (int<dlv>:message:boxed.xml :end func/name)))
+
+
+(defun dlv:cmd:dir-locals/show/all (filepath)
+  "Show all DLVs for FILEPATH.
+
+Output to '*Messages*' buffer."
   (interactive (list (read-directory-name "Path: "
                                           buffer-file-name)))
-  (let (dlv.classes
+  (let ((func/name "dlv:cmd:buffer-locals:show/all")
+        ;; (func/tags '(:cmd :info))
+        dlv.classes
         dlv.dir-cache
         dlv.class-alist
         dlv.safe-local-vars)
@@ -159,7 +187,7 @@ A very much reduced list from `dlv:buffer-locals/show-all'."
       (setq dlv.class-alist (funcall indent (funcall char-to-str dlv.class-alist)))
       (setq dlv.safe-local-vars (funcall indent (funcall char-to-str dlv.safe-local-vars))))
 
-    (int<dlv>:message:boxed.xml :start "dlv:dir-locals/show-all" (cons "path" filepath))
+    (int<dlv>:message:boxed.xml :start func/name (cons "path" filepath))
     (message "")
 
     (message "`enable-local-variables': %S"
@@ -180,56 +208,88 @@ A very much reduced list from `dlv:buffer-locals/show-all'."
     (int<dlv>:message:line ?─)
     (message "`safe-local-variable-values':\n%s"
              dlv.safe-local-vars)
-    (int<dlv>:message:boxed.xml :end "dlv:dir-locals/show-all")))
+    (int<dlv>:message:boxed.xml :end func/name)))
 
 
 ;;------------------------------------------------------------------------------
 ;; Messaging Helpers
 ;;------------------------------------------------------------------------------
-;; TODO: move all of these to mis?
+;; TODO: move all of these to mis/nub/whatever?
 ;;---
 
 (defmacro int<dlv>:message:stream.chars (stream)
-  "Returns a lambda that stores supplied char into the STREAM variable."
+  "Return a lambda that will store the supplied char into the STREAM variable."
   `(let ((int<dlv>:message:stream.chars:stream ,stream))
      (lambda (char) (setq int<dlv>:message:stream.chars:stream
                           (cons char int<dlv>:message:stream.chars:stream)))))
 
 
 (defun int<dlv>:message:stream->str (stream)
-  "Converts the character STREAM into a string destructive (uses `nreverse')."
+  "Convert the character STREAM into a string destructively.
+
+Uses `nreverse' on STREAM."
   (concat (nreverse stream)))
 
 
+;; TODO: Should this trim (just) before returing? Won't that mess up the first indent?
 (defun int<dlv>:message:indent (str &optional indent)
   "Indent each line in STR by INDENT amount (default 2).
 
-Trims string of leading/trailing whitespace before returning."
+Trim string of leading/trailing whitespace before returning."
   (let* ((indent.amt (or indent 2))
          (indent.fmt (concat "%" (number-to-string indent.amt) "s"))
          string.indented)
     (string-trim
      (dolist (line (split-string str "\n") string.indented)
-       (setq indented (concat indented
-                              (format indent.fmt line)
-                              "\n"))))))
+       (setq string.indented (concat string.indented
+                                     (format indent.fmt line)
+                                     "\n"))))))
 
 
-(defun int<dlv>:message:line (char)
-  "Print an ASCII box line to the *Messages* buffer."
-  (let ((width.usable 80))
+(defun int<dlv>:message:line (char &optional width)
+  "Print a line of CHAR to the '*Messages*' buffer.
+
+If WIDTH is a positive integer, output line will be WIDTH wide.
+The default, otherwise, is 80.
+
+(int<dlv>:message:line ?─ 10)
+  -> \"\n──────────\n\"
+(int<dlv>:message:line ?- 10)
+  -> \"\n----------\n\"
+(int<dlv>:message:line ?x 10)
+  -> \"\nxxxxxxxxxx\n\""
+  ;; Figure out what width to use.
+  (let ((width.usable (if (and (integerp width) (> width 0))
+                          width
+                       80)))
     (message "\n%s\n"
              (make-string width.usable char))))
 
 
 (defun int<dlv>:message:boxed.xml (start? title &rest kvp)
-  "Print an ASCII boxed message to the *Messages* buffer that is XML-ish formatted.
+  "Print a message boxed in an ASCII double-line box.
 
+Vaguely XML-themed format.
+
+If START? is non-nil, it will be a start tag.
+  \"<title\" [...]
 If START? is nil or `:end', it will be an end tag.
+  \"</title\" [...]
 
-TITLE will be the 'XML tag name'.
+TITLE will be the XML 'tag name'.
 
-KVP, if not nil, should be 2-tuples (cons) of field-name and field-value strings."
+KVP, if not nil, should be 2-tuples (cons) of field-name and field-value
+strings.
+
+Example:
+\(int<dlv>:message:boxed.xml t \"testing\"
+                            (cons \"greeting\" \"hello\")
+                            (cons \"subject\" \"world\"))
+╔══════════════════════════════════════════════════════════════════════════════╗
+║ <testing                                                                     ║
+║   greeting=\"hello\"                                                           ║
+║   subject=\"world\">                                                           ║
+╚══════════════════════════════════════════════════════════════════════════════╝"
   (let* ((width.total 80)
          (width.sides 2)
          (width.padding 2)
@@ -266,6 +326,7 @@ KVP, if not nil, should be 2-tuples (cons) of field-name and field-value strings
                "║"
                (format format.width.title
                        (concat "<"
+                               (if (memq start? '(nil :end)) "/" "")
                                title
                                ;; Don't close the tag.
                                " "))
@@ -294,12 +355,12 @@ KVP, if not nil, should be 2-tuples (cons) of field-name and field-value strings
              line.width.middle
              "╝")
     nil))
-;; (int<dlv>:message:boxed.xml "testing")
-;; (int<dlv>:message:boxed.xml "testing" (cons "hello" "there"))
-;; (int<dlv>:message:boxed.xml "testing" (cons "hello" "there") (cons "general" "kenobi"))
+;; (int<dlv>:message:boxed.xml nil "testing")
+;; (int<dlv>:message:boxed.xml nil "testing" (cons "hello" "there"))
+;; (int<dlv>:message:boxed.xml t "testing" (cons "greeting" "hello") (cons "subject" "world"))
 
 
 ;;------------------------------------------------------------------------------
 ;; The End.
 ;;------------------------------------------------------------------------------
-(imp:provide:with-emacs :dlv 'display)
+(imp:provide :dlv 'display)
