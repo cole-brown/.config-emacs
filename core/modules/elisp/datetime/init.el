f4361ee42093d1184921a9a41888c1cdac9dc693
Cole Brown <code@brown.dev>
2022-06-01 09:38:02 -0700

    Refactor "datetime/init.el"

--- core/modules/elisp/datetime/init.el
+++ core/modules/elisp/datetime/init.el
@@ -1,17 +1,26 @@
-;;; spy/datetime/init.el -*- lexical-binding: t; -*-
+;;; elisp/datetime/init.el -*- lexical-binding: t; -*-
+
+
+;;------------------------------------------------------------------------------
+;; Set-Up.
+;;------------------------------------------------------------------------------
+
+(imp:path:root :datetime
+               (imp:path:current:dir)
+               (imp:file:current))
 
 
 ;;------------------------------------------------------------------------------
 ;; Load our sub-module thingies.
 ;;------------------------------------------------------------------------------
 
-(imp:load :feature  '(:modules spy datetime format)
+(imp:load :feature  '(:datetime format)
           :filename "format")
-(imp:load :feature  '(:modules spy datetime timestamp)
+(imp:load :feature  '(:datetime timestamp)
           :filename "timestamp")
 
 
 ;;------------------------------------------------------------------------------
 ;; The End.
 ;;------------------------------------------------------------------------------
-(imp:provide :modules 'spy 'datetime)
+(imp:provide:with-emacs :datetime)
