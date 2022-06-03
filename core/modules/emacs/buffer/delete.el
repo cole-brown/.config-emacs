57341f0746dbf0884bd8c40f65db73dbe5a1e8e2
Cole Brown <code@brown.dev>
2022-06-03 11:12:34 -0700

    Rename buffer function
    It isn't a private function, so make it a non-private name.

--- core/modules/emacs/buffer/delete.el
+++ core/modules/emacs/buffer/delete.el
@@ -2,7 +2,7 @@
 
 
 
-(defun int<buffer>:delete:word (arg)
+(defun buffer:delete:word (arg)
   "Delete ARG number of words without putting them in the kill ring.
 
 Kill characters forward until encountering the end of a word."
