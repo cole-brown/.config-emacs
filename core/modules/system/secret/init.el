2f2961cda2f8cd439b97a639a6cd150e69ca16d0
Cole Brown <code@brown.dev>
2022-06-07 09:17:24 -0700

    Refactor "system/secret/init.el"

--- core/modules/system/secret/init.el
+++ core/modules/system/secret/init.el
@@ -1,4 +1,33 @@
-;;; spy/secret/init.el -*- lexical-binding: t; -*-
+;;; system/secret/init.el -*- lexical-binding: t; -*-
+;;
+;; Author:   Cole Brown <code@brown.dev>
+;; URL:      https://github.com/cole-brown/.config-emacs
+;; Created:  2020-08-28
+;; Modified: 2022-06-05
+;;
+;; These are not the GNU Emacs droids you're looking for.
+;; We can go about our business.
+;; Move along.
+;;
+;;; Commentary:
+;;
+;; Allow multiple systems (computers) to use the same init with small
+;; differences.
+;;
+;;
+;; NOTE: User must call both:
+;;   1. `spy:secret:init'
+;;   2. `spy:secret:config'
+;;
+;; They should be called in user's init code before anything that may require
+;; their secrets.
+;;
+;; `spy:secret:init' must be called very early on.
+;;
+;; `spy:secret:config' could be called just after `spy:secret:init', or later
+;; on in config, depending on how you use your secrets' init/config.
+;;
+;; Existance of secrets for this system is first checked for in `spy:secret:init'.
 ;;
 ;;; Code:
 
@@ -9,36 +38,21 @@
 
 
 ;;------------------------------------------------------------------------------
-;; :spy:secret requirements
+;; Load our files.
 ;;------------------------------------------------------------------------------
 
-(imp:load :feature  '(:modules spy secret functions)
-          :filename "functions")
-(imp:load :feature  '(:modules spy secret load)
-          :filename "load")
+(imp:timing
+    '(:system secret)
+    (imp:file:current)
+    (imp:path:current:dir)
 
-
-;;------------------------------------------------------------------------------
-;; Notes
-;;------------------------------------------------------------------------------
-;;
-;; User must call both:
-;;   `spy:secret:init'
-;;   `spy:secret:config'
-;;
-;; They should be called in .doom.d/config.el, before anything that may require
-;; their secrets.
-;;
-;; `spy:secret:init' must be called very early on.
-;;
-;; `spy:secret:config' could be called just after `spy:secret:init', or later
-;; on in config, depending on how you use your secrets' init/config.
-
-
-;; Existance of secrets for this system is first checked for in `spy:secret:init'.
+  (imp:load :feature  '(:system secret functions)
+            :filename "functions")
+  (imp:load :feature  '(:system secret load)
+            :filename "load"))
 
 
 ;;------------------------------------------------------------------------------
 ;; The End.
 ;;------------------------------------------------------------------------------
-(imp:provide :modules 'spy 'secret)
+(imp:provide :system 'secret)
