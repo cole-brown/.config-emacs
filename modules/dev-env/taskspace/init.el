9ab1b3235b8aeb8403130688a2825696f419acb7
Cole Brown <code@brown.dev>
2022-07-06 11:02:34 -0700

    Refactor "taskspace.el": keybinds
    Split functionality related to keybinds from "taskspace.el" and put into
    new "keybinds.el" file.
    
    Move Taskspace's commentary from "taskspace.el" to "init.el".

--- modules/dev-env/taskspace/init.el
+++ modules/dev-env/taskspace/init.el
@@ -3,8 +3,8 @@
 ;; Author:     Cole Brown <http://github/cole-brown>
 ;; Maintainer: Cole Brown <code@brown.dev>
 ;; Created:    2019-04-24
-;; Modified:   2022-07-01
-;; Version: 2.2
+;; Modified:   2022-07-06
+;; Version:    2.3
 ;;
 ;; These are not the GNU Emacs droids you're looking for.
 ;; We can go about our business.
@@ -12,9 +12,187 @@
 ;;
 ;;; Commentary:
 ;;
-;; Per-task notes & files.
+;; taskspace.el is a KISS taskspace (workspace) generator/manager.
 ;;
-;; Initialize the taskspace module.
+;; FAQ:
+;;   1) Can it do X?
+;;      - No... I really meant simple.
+;;   2) Can it do multiple taskspace roots?
+;;      - Yes... I had to make it less simple.
+;;
+;; It can make a folder based on a simple dating and numbering scheme, with a
+;; simple description tacked on for human usability.
+;;
+;; It can copy files into the new taskspace. It can generate files based on a
+;; static string or a function supplied in taskspace vars.
+;;
+;; It can copy the taskspace's full path or name to the kill ring/clipboard.
+;;
+;; It can open the taskspace dir itself (or the taskspace parent dir)
+;; in a buffer.
+;;
+;; It can 'deal' with these kind of taskspaces:
+;;   - Self-Contained
+;;;    - Taskspace dir contains:
+;;       - notes,
+;;       - data,
+;;       - etc.
+;;    - No other directories or files.
+;;   - Split (taskspace notes are separate; taskspace dir contains data, etc)
+;;;    - Taskspace dir contains:
+;;       - data,
+;;       - etc.
+;;    - Notes file exists separately.
+;;
+;;
+;;------------------------------
+;; Commands:
+;;------------------------------
+;;   The Main Command:
+;;     `taskspace:create'
+;;       - Create a new taskspace. Will prompt for the short description.
+;;         - e.g. description of "2019-01-01_2_some-task-name" is
+;;           "some-task-name".
+;;
+;;   DWIM Commands:
+;;     - Accepts numeric prefix arg.
+;;       - 0 or no prefix: Today's date
+;;       - positive: Future date; N days from now.
+;;       - negative: Past date; N days back.
+;;     - DWIM means:
+;;       - If none for date: Create.
+;;       - If just one existing: Return it.
+;;       - If multiple: Choose from prompt of options.
+;;     `taskspace:dwim:dir'
+;;       - Returns fully qualified path to taskspace.
+;;         - e.g. "c:/home/user/taskspace/2019-01-01_2_some-task-name"
+;;     `taskspace:dwim:name'
+;;       - Returns taskspace (directory) name only.
+;;         - e.g. "2019-01-01_2_some-task-name"
+;;
+;;   Other Commands:
+;;     `taskspace:dired:task'
+;;       - Opens the directory of a task in emacs
+;;         (uses find-file, so defaults to dired-mode buffer).
+;;     `taskspace:dired:root'
+;;       - Opens the directory of all tasks in emacs (aka `(int<taskspace>:config group :dir/tasks)')
+;;         (uses find-file, so defaults to dired-mode buffer).
+;;     `taskspace:shell'
+;;       - Opens a shell.
+;;       - Use `taskspace:dwim:dir' to determine which taskspace is
+;;         intended from the context.
+;;
+;;
+;;------------------------------
+;; Settings:
+;;------------------------------
+;; See 'General Settings' header to find out what all can be customized per
+;; group right now.
+;;
+;;
+;;------------------------------
+;; Use-Package Config, Simple:
+;;------------------------------
+;;
+;;  (use-package taskspace)
+;;
+;;
+;;------------------------------
+;; Use-Package Config, Multi-Group:
+;;------------------------------
+;;
+;; (use-package taskspace
+;;   ;; My own personal package - do not package manager it.
+;;   :ensure nil
+;;
+;;   ;;------------------------------
+;;   :init
+;;   ;;------------------------------
+;;
+;;   ;;---
+;;   ;; General (Non-Per-Domain) Init...
+;;   ;;---
+;;   (defun my/taskspace/generate (taskname taskpath)
+;;     "NOTE: Could be redefined later for more work-specific details, so check
+;; e.g. 'finalize-domain-secret.el' for a redef. Or 'C-h f
+;; my/taskspace/generate' and see what file it's defined
+;; in.
+;; "
+;;     ;; Format:
+;;     ;; header snippet key
+;;     ;;
+;;     ;; taskname
+;;     ;; taskpath
+;;     ;;
+;;     ;; 'mkdir cmd'
+;;     ;;
+;;     ;; fancy box to separate this stuff from start of normal notes
+;;     (format (concat "%s\n" ;; header
+;;                     "\n"
+;;                     "#+TASKSPACE: %s\n" ;; taskpath
+;;                     "%s\n" ;; taskname
+;;                     "\n"
+;;                     "%s\n" ;; mkdir cmd for remote servers
+;;                     "\n"
+;;                     "%s\n" ;; fancy box top
+;;                     "%s\n" ;; fancy box middle
+;;                     "%s\n" ;; fancy box bottom
+;;                     "\n\n")
+;;             "my-header-snippet"
+;;             taskpath
+;;             taskname
+;;             (format "mkdir ~/temp/%s" taskname)
+;;             "     ┌┬┬┬──────────────────────────────────────────────────────────────┬┬┬┐"
+;;             "     ├┼┼┤                             ...                              ├┼┼┤"
+;;             "     └┴┴┴──────────────────────────────────────────────────────────────┴┴┴┘"
+;;             ))
+;;
+;;   ;;---
+;;   ;; "Home" Domain
+;;   ;;---
+;;
+;;   ;; I can redef later if I want different ones..
+;;   (defalias 'my/taskspace/generate/home 'my/taskspace/generate)
+;;
+;;   (defvar my/taskspace/group/home
+;;     '((:type/notes        :self-contained)
+;;       (:format/datetime   my/datetime/format/yyyy-mm-dd)
+;;       (:dir/tasks         my/taskspace/path/tasks/home)
+;;       (:dir/notes         my/taskspace/path/notes/home)
+;;       (:file/new/generate ((".projectile" "") ;; projectile: empty file
+;;                            ;; notes.org: setup with org header snippet
+;;                            ;; ready to go
+;;                            ((int<taskspace>:config :home :file/notes)
+;;                             my/taskspace/generate/home))))
+;;     "Custom settings for my `:home' taskspace group.")
+;;
+;;   ;;---
+;;   ;; "Work" Domain
+;;   ;;---
+;;
+;;   ;; I can redef later if I want different ones..
+;;   (defalias 'my/taskspace/generate/work 'my/taskspace/generate)
+;;
+;;   (defvar my/taskspace/group/work
+;;     '((:type/notes        :noteless)
+;;       (:format/datetime   my/datetime/format/yyyy-mm-dd)
+;;       (:dir/tasks         my/taskspace/path/tasks/work)
+;;       (:dir/notes         my/taskspace/path/notes/work)
+;;       (:file/new/generate ((".projectile" "") ;; projectile: empty file
+;;                            ;; notes.org: setup with org header snippet
+;;                            ;; ready to go
+;;                            ((int<taskspace>:config :home :file/notes)
+;;                             my/taskspace/generate/home))))
+;;     "Custom settings for my `:work' taskspace group.")
+;;
+;;   ;;------------------------------
+;;   :custom
+;;   ;;------------------------------
+;;
+;;   (taskspace:groups
+;;    '((:work    "Work Taskspace" my/taskspace/group/work)
+;;      (:home    "Home Taskspace" my/taskspace/group/home)
+;;      (:default "Defaults"       taskspace:group:default))))
 ;;
 ;;; Code:
 
@@ -47,6 +225,9 @@
 
   (imp:load :feature  '(:taskspace taskspace)
             :filename "taskspace")
+
+  (imp:load :feature  '(:taskspace keybinds)
+            :filename "keybinds")
 
   ;; End load timing.
   )
---
+++ modules/dev-env/taskspace/keybinds.el
@@ -0,0 +1,176 @@
+;;; keybinds.el --- Keybindings -*- lexical-binding: t; -*-
+;;
+;; Author:     Cole Brown <http://github/cole-brown>
+;; Maintainer: Cole Brown <code@brown.dev>
+;; Created:    2022-07-06
+;; Modified:   2022-07-06
+;;
+;; These are not the GNU Emacs droids you're looking for.
+;; We can go about our business.
+;; Move along.
+;;
+;;; Commentary:
+;;
+;; Keybinds for Taskspace
+;;
+;;; Code:
+
+
+;;---------------------------------taskspace------------------------------------
+;;--                   Simple Taskspace / Task Management                     --
+;;------------------------------------------------------------------------------
+
+
+;;------------------------------------------------------------------------------
+;; DOOM!
+;;------------------------------------------------------------------------------
+
+;; TODO: more options for making keybind:
+;; (&optional orphaned description prefix)
+(defun taskspace:keybind:doom ()
+  "Create keybinds in Doom, or raise an error if not in Doom.
+
+Create a keymap; insert into doom/evil or vanilla Emacs as
+appropriate/parameters say.
+
+Creates the taskspace keymap under the doom leader key (default SPC)
+TODO: unless ORPHANED is not nil.
+
+TODO: Uses DESCRIPTION if it is a string, else the description is 'taskspace'.
+
+TODO: Uses PREFIX as the keymap prefix if not nil, else tries to use:
+TODO:   - doom/evil emacs: doom-leader(?) n T
+TODO:   - vanilla emacs:   TODO
+TODO:
+TODO: PREFIX for doom/evil should be either:
+TODO:   1) a key string:
+TODO:      - \"t\"
+TODO:      - \"n t\"
+TODO:        - \"nt\" is equivalent
+TODO:   2) a cons of (key-string . description-str):
+TODO:      - (\"t\" . \"taskspace\")
+TODO:      - (\"n t\" . \"taskspace\")
+TODO:   3) a list of 1 or 2 for setting into a sub-map:
+TODO:      - (\"t\" (\"n\" (\"t\" . \"taskspace\")))"
+  (if (null (symbolp 'doom!)) ;; Doom's loading function should mean this is doom?
+      (nub:error
+          :taskspace
+          "taskspace:keybind:doom"
+        "We are not in a Doom Emacs environment..? Cannot set Doom Emacs keybinds.")
+
+    ;; Map under the Doom leader, probably.
+    (map! :leader
+          ;; Give it a description...
+          :desc "taskspace"
+
+          ;; ...and give it keybinds.
+          (:prefix ("T" . "taskspace")
+
+           ;; Top level commands...
+           :desc "Create new..."  "T" #'taskspace:create
+           :desc "Visit notes..." "v" #'taskspace:notes
+           :desc "Shell..."       "s" #'taskspace:shell
+
+           ;; 'Copy to kill ring' functions:
+           (:prefix ("k" . "Kill...")
+
+            :desc "dir"  "k" #'taskspace:dwim:dir
+            :desc "name" "n" #'taskspace:dwim:name)
+
+           ;; 'Open a dired buffer' functions:
+           (:prefix ("d" . "dired")
+            :desc "task dired buffer" "d" #'taskspace:dired:task
+            :desc "root dired buffer" "r" #'taskspace:dired:root)))))
+;; TODO: prefix: ;; Put it under the prefix(es).
+;; TODO: prefix: (cond
+;; TODO: prefix:  ;;---
+;; TODO: prefix:  ;; NIL
+;; TODO: prefix:  ;;---
+;; TODO: prefix:  ;; Return just the keybinds.
+;; TODO: prefix:  ((null prefix)
+;; TODO: prefix:   (int<taskspace>:keybind/doom))
+;; TODO: prefix:
+;; TODO: prefix:  ;;---
+;; TODO: prefix:  ;; STRING
+;; TODO: prefix:  ;;---
+;; TODO: prefix:  ;; Put keybinds under the prefix str.
+;; TODO: prefix:  ((stringp prefix)
+;; TODO: prefix:   (list prefix (int<taskspace>:keybind/doom)))
+;; TODO: prefix:
+;; TODO: prefix:  ;;---
+;; TODO: prefix:  ;; CONS
+;; TODO: prefix:  ;;---
+;; TODO: prefix:  ;; Put keybinds under the prefix /CONS/.
+;; TODO: prefix:  ;; ONLY CONS!!!
+;; TODO: prefix:  ;; No lists.
+;; TODO: prefix:  ;; Just cons.
+;; TODO: prefix:  ;; https://emacs.stackexchange.com/questions/10489/predicate-function-for-dotted-pairs
+;; TODO: prefix:  ((and (cdr var) (atom (cdr var)))
+;; TODO: prefix:   ;; Create list with description, key, and our keybids.
+;; TODO: prefix:   (list
+;; TODO: prefix:    :desc (nth 1 prefix)
+;; TODO: prefix:    (nth 0 prefix)
+;; TODO: prefix:    (int<taskspace>:keybind/doom)))
+;; TODO: prefix:
+;; TODO: prefix:  ;;---
+;; TODO: prefix:  ;; LIST
+;; TODO: prefix:  ;;---
+;; TODO: prefix:  ((listp prefix)
+;; TODO: prefix:   ;; Build first layer and do a recursion...
+;; TODO: prefix:   (-let [(this-prefix . rest) prefix]
+;; TODO: prefix:     (
+;; TODO: prefix:      )))
+;; TODO: prefix:
+;; TODO: prefix:   (list
+;; TODO: prefix:    :desc (nth 1 prefix)
+;; TODO: prefix:    (nth 0 prefix)
+;; TODO: prefix:
+;; TODO: prefix:    prefix (int<taskspace>:keybind/doom)))
+;; TODO: prefix:
+;; TODO: prefix:
+;; TODO: prefix: (defun int<taskspace>:no-error-predicate (func value)
+;; TODO: prefix:   "Checks if VALUE fulfills FUNC function predicate.
+;; TODO: prefix:
+;; TODO: prefix: Wraps check in a `condition-case-unless-debug', returns nil if error signal caught."
+;; TODO: prefix:   (condition-case-unless-debug nil
+;; TODO: prefix:       (funcall func value)
+;; TODO: prefix:     (error nil)))
+;; TODO: prefix:
+;; TODO: prefix:
+;; TODO: prefix: (defun int<taskspace>:keybind/doom.level (key description rest)
+;; TODO: prefix:   "Creates a level of a doom `map!'.
+;; TODO: prefix:
+;; TODO: prefix: KEY must be a string for the key(s) to use for this level of the keybind tree.
+;; TODO: prefix:
+;; TODO: prefix: DESCRIPTION must be a (short) description string or nil.
+;; TODO: prefix:
+;; TODO: prefix: REST must be a function to call, or a deeper (child) level of the keybind tree."
+;; TODO: prefix:   ;; Error check our inputs...
+;; TODO: prefix:   (cond ((not (int<taskspace>:no-error-predicate #'stringp key))
+;; TODO: prefix:          (error "%s: Key must be a string: %S"
+;; TODO: prefix:                 "int<taskspace>:keybind/doom.level" key))
+;; TODO: prefix:
+;; TODO: prefix:         ((and (not (null description))
+;; TODO: prefix:               (not (int<taskspace>:no-error-predicate #'stringp description)))
+;; TODO: prefix:          (error "%s: Description must be nil or a string: %S"
+;; TODO: prefix:                 "int<taskspace>:keybind/doom.level" description))
+;; TODO: prefix:
+;; TODO: prefix:         ((null rest)
+;; TODO: prefix:          (error "%s: Must have something to bind: %S"
+;; TODO: prefix:                 "int<taskspace>:keybind/doom.level" rest))
+;; TODO: prefix:
+;; TODO: prefix:         ;;---
+;; TODO: prefix:         ;; Success - make the level.
+;; TODO: prefix:         ;;---
+;; TODO: prefix:         (t
+;; TODO: prefix:          (if (null description)
+;; TODO: prefix:              (list key rest)
+;; TODO: prefix:            (list :desc description
+;; TODO: prefix:                  key
+;; TODO: prefix:                  rest)))))
+
+
+;;------------------------------------------------------------------------------
+;; The End.
+;;------------------------------------------------------------------------------
+(imp:provide :taskspace 'keybinds)
