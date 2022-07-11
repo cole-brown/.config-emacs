a48c896c22a700a30458877bbfb5df034d749ae8
Cole Brown <code@brown.dev>
2022-07-11 09:52:20 -0700

    New/bare init files for evil
    `:input` module "init.el" for `:input` root dir.
    
    `:input:keyboard:evil` "init.el" as well.
    
    Neither really do anything. This is intentional in the case of
    "input/init.el", and we'll get to adding things to
    "input/keyboard/evil/init.el" soon.

---
+++ modules/input/init.el
@@ -0,0 +1,37 @@
+;;; init.el --- `:input' namespace -*- lexical-binding: t; -*-
+;;
+;; Author:   Cole Brown <code@brown.dev>
+;; URL:      https://github.com/cole-brown/.config-emacs
+;; Created:  2022-07-11
+;; Modified: 2022-07-11
+;;
+;; These are not the GNU Emacs droids you're looking for.
+;; We can go about our business.
+;; Move along.
+;;
+;;; Commentary:
+;;
+;;  `:input' namespace initialization
+;;
+;;; Code:
+
+
+;;------------------------------------------------------------------------------
+;; Set-Up.
+;;------------------------------------------------------------------------------
+
+(imp:path:root :input
+               (imp:path:current:dir))
+
+
+;;------------------------------------------------------------------------------
+;; Loading.
+;;------------------------------------------------------------------------------
+
+;; Don't load any of the `:input' modules - let the user decided what to do.
+
+
+;;------------------------------------------------------------------------------
+;; The End.
+;;------------------------------------------------------------------------------
+(imp:provide :input)
