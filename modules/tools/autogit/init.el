cf629255a1b437dab8fc181c5c3a70e29275525b
Cole Brown <code@brown.dev>
2022-07-25 10:29:04 -0700

    Refactor "autogit/init.el"

--- modules/tools/autogit/init.el
+++ modules/tools/autogit/init.el
@@ -1,19 +1,18 @@
 ;;; tools/autogit/init.el --- auto-commit git repos -*- lexical-binding: t; -*-
 ;;
-;; Copyright (C) 2021 Cole Brown
-;; TODO: MIT or GPL dual license?
-;; TODO: full header?
-;;
-;; Author: Cole Brown <http://github/cole-brown>
+;; Author:     Cole Brown <http://github/cole-brown>
 ;; Maintainer: Cole Brown <code@brown.dev>
-;; Created: October 28, 2020
-;; Modified: October 28, 2020
-;; Version: 0.9.0
-;; Keywords: vc tools
-;; Homepage: https://github.com/cole-brown/.config-secret
+;; Created:    2020-10-28
+;; Modified:   2020-10-28
+;; URL:        https://github.com/cole-brown/.config-emacs
+;; Version:    1.0.0
+;; Keywords:   vc tools
+;; Homepage:   https://github.com/cole-brown/.config-secret
 ;; Package-Requires: ((emacs "27.1") (magit "3.3.0"))
 ;;
-;; This file is not part of GNU Emacs.
+;; These are not the GNU Emacs droids you're looking for.
+;; We can go about our business.
+;; Move along.
 ;;
 ;;; Commentary:
 ;;
@@ -30,15 +29,8 @@
 
 
 ;;------------------------------------------------------------------------------
-;; Usage
-;;------------------------------------------------------------------------------
-
-
-
-;;------------------------------------------------------------------------------
 ;; Custom Variables
 ;;------------------------------------------------------------------------------
-
 
 ;;------------------------------
 ;; Group
@@ -56,38 +48,41 @@
 
 (defcustom autogit:repos:path/commit
   nil
-  "List of strings of directories to automatically add/commit/push in their
+  "Repo paths to commit.
+
+List of strings of directories to automatically add/commit/push in their
 respective git repos. Can be sub-directories of a repository (TODO: add
 allowance for single files?)."
   :group 'autogit:group
-  :type '(restricted-sexp :tag "String List"
-                          :match-alternatives
-                          (lambda (x) (and (listp x)
-                                           (seq-every-p #'stringp x)))))
+  :type  '(restricted-sexp :tag "String List"
+                           :match-alternatives
+                           (lambda (x) (and (listp x)
+                                            (seq-every-p #'stringp x)))))
 
 
 (defcustom autogit:repos:path/watch
   nil
-  "List of strings of directories to watch for (not necessarily auto-commit)
-changes. Can be sub-directories of a repository (TODO: add allowance for single
-files?)."
+  "Repo paths to watch (status).
+
+List of strings of directories to watch for (not necessarily auto-commit)
+changes. Can be sub-directories of a repository."
   :group 'autogit:group
-  :type '(restricted-sexp :tag "String List"
-                          :match-alternatives
-                          (lambda (x) (and (listp x)
-                                           (seq-every-p #'stringp x)))))
+  :type  '(restricted-sexp :tag "String List"
+                           :match-alternatives
+                           (lambda (x) (and (listp x)
+                                            (seq-every-p #'stringp x)))))
 
 
 ;;------------------------------
 ;; Timestamp
 ;;------------------------------
 
-;; Default to ISO-8601 with space separator for in text.
+;; Default to RFC-3339 (aka ISO-8601-ish) with space separator for in text.
 (defcustom autogit:datetime:format
   "%Y-%m-%d %H:%M:%S"
   "Datetime format string for autogit messages."
   :group 'autogit:group
-  :type 'string)
+  :type  'string)
 
 
 ;;------------------------------
@@ -96,29 +91,29 @@ files?)."
 
 (defcustom autogit:changes:symbols
   '((:staged    . "•")
-    (:unstaged  . "+")  ;; too wide in current font/theme: ✚
-    (:untracked . "¬")  ;; acceptable: ?
+    (:unstaged  . "+")   ;; too wide in current font/theme: ✚
+    (:untracked . "¬")   ;; acceptable: ?
     (:unmerged  . "⊥")) ;; acceptable: ×, too wide in current font/theme: ✖
   "Symbol/icon/character to use to represent change type."
   :group 'autogit:group
-  ;; TODO: type is... what? stupid types.
-  :type '(alist :key-type symbol :value-type string))
+  :type  '(alist :key-type symbol :value-type string))
 ;; (makunbound 'autogit:changes:symbols)
 
 
 (defcustom autogit:changes:display
   :full
-  "What to display for status of each watch location during `autogit:repos:watch'.
+  "What to display for watched repo (`autogit:repos:watch') statuses.
 
 Options:
-  - :summary - One line of number of files staged/unstaged/etc. See `autogit:changes:symbols'.
+  - :summary - One line of symbols & number of files staged/unstaged/etc.
+               See `autogit:changes:symbols'.
   - :paths   - List of lists. Type of change (staged/etc) -> file paths.
   - :full    - Summary followed by paths."
   :group 'autogit:group
-  ;; TODO: type is... what? stupid types.
-  :type '(choice (const :tag "Summary: One line of number of files staged/unstaged/etc. See `autogit:changes:symbols'" :summary)
-                 (const :tag "Paths: List of lists. Type of change (staged/etc) -> file paths." :paths)
-                 (const :tag "Full: 'Summary' followed by 'paths'." :full)))
+  :type  '(choice (const :tag "Summary: One line of number of files staged/unstaged/etc. See `autogit:changes:symbols'" :summary)
+                  (const :tag "Paths: List of lists. Type of change (staged/etc) -> file paths." :paths)
+                  (const :tag "Full: 'Summary' followed by 'paths'." :full)))
+
 
 ;;------------------------------
 ;; Buffer
@@ -127,29 +122,33 @@ Options:
 (defcustom autogit:buffer:display? t
   "Should autogit display the push/status buffers when it starts outputting?"
   :group 'autogit:group
-  :type '(boolean))
+  :type  '(boolean))
 
 
 (defcustom autogit:buffer:switch? t
   "Should autogit switch to the push/status buffers when it completes?"
   :group 'autogit:group
-  :type '(boolean))
+  :type  '(boolean))
 
 
 (defcustom autogit:buffer:name/push
   "ⓘ-autogit:push-ⓘ"
-  "Buffer name to print to. If you want it to go to *Messages* with the usual
-minibuffer interaction, set to: `:messages'"
+  "Buffer name to print to.
+
+If you want it to go to *Messages* with the usual minibuffer interaction, set
+to: `:messages'"
   :group 'autogit:group
-  :type '(choice (string :tag "Name of Buffer")
-                 (const :tag "Use `message' to send to *Messages* buffer with the usual minibuffer interactions."
-                        :messages)))
+  :type  '(choice (string :tag "Name of Buffer")
+                  (const :tag "Use `message' to send to *Messages* buffer with the usual minibuffer interactions."
+                         :messages)))
 
 
 (defcustom autogit:buffer:name/status
   "ⓘ-autogit:status-ⓘ"
-  "Buffer name to print to. If you want it to go to *Messages* with the usual
-minibuffer interaction, set to: `:messages'"
+  "Buffer name to print to.
+
+If you want it to go to *Messages* with the usual minibuffer interaction, set
+to: `:messages'"
   :group 'autogit:group
   :type '(choice (string :tag "Name of Buffer")
                  (const :tag "Use `message' to send to *Messages* buffer with the usual minibuffer interactions."
@@ -164,7 +163,7 @@ minibuffer interaction, set to: `:messages'"
   "[AUTOGIT]"
   "Name of this package, used in output messages.
 
-Set to `nil' if not desired in output.")
+Set to nil if not desired in output.")
 ;; (makunbound 'autogit:text:name)
 
 
@@ -175,17 +174,17 @@ Set to `nil' if not desired in output.")
 Dry-Runs won't actually do some of the git calls, as a dry-run
 should not result in anything changing.
 
-Set to `nil' if not desired in output.")
+Set to nil if not desired in output.")
 ;; (makunbound 'autogit:text:dry-run)
 
 
 (defcustom autogit:text:properties
   '(;; General
     :face:self      (face package-name) ;; font-lock-keyword-face ; for `autogit:text:name'
-    :face:title     (face outline-1) ;; font-lock-function-name-face
-    :face:highlight (face outline-2) ;; font-lock-variable-name-face
-    :face:path      (face outline-3) ;; font-lock-doc-face
-    :face:git       (face outline-4) ;; font-lock-preprocessor-face
+    :face:title     (face outline-1)    ;; font-lock-function-name-face
+    :face:highlight (face outline-2)    ;; font-lock-variable-name-face
+    :face:path      (face outline-3)    ;; font-lock-doc-face
+    :face:git       (face outline-4)    ;; font-lock-preprocessor-face
 
     ;; Status
     :face:error   (face error) ;; font-lock-string-face
