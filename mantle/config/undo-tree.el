7e7cca8f5bebb252ef90ebe27202d68aaa93591c
Cole Brown <code@brown.dev>
2022-08-02 09:09:46 -0700

    Add to `undo-tree` config from Doom
    Investigating why `undo-tree` asks to open a file when an org file is
    opened. Found Doom's `undo-tree` config and there was a lot more there.
    
    Add setting some defcustoms.
    
    Add setting some of Emacs' undo-related defcustoms for undo history size
    to much larger values since:
      a) Much RAM; very wow.
      b) `undo-tree` generates a lot more/larger histories.
    
    Add compression of history files if `zstd` or `gzip` exes are on the
    system.
    
    Add stripping of text properties from undo tree.
    
    Add new `innit:advice:squelch` around the `undo-tree-save-history`
    function to get rid of the chatter.

--- mantle/config/undo-tree.el
+++ mantle/config/undo-tree.el
@@ -23,25 +23,77 @@
 ;; Undo-Tree
 ;;------------------------------------------------------------------------------
 
-(imp:use-package undo-tree
-  :demand t ;; Always load.
+(let ((file/this (imp:file:current))
+      (tags/this '(:innit :mantle :undo :innit)))
+  (imp:use-package undo-tree
+    :demand t ;; Always load.
 
-  ;;--------------------
-  :init
-  ;;--------------------
+    ;;--------------------
+    :init
+    ;;--------------------
 
-  ;; Declare that we'll have `undo-tree' available as the `evil' undo feature.
-  ;; https://github.com/emacs-evil/evil#dependencies
-  (imp:flag :evil +undo-tree)
-
-  ;;--------------------
-  :config
-  ;;--------------------
-
-  (global-undo-tree-mode +1))
+    ;; Declare that we'll have `undo-tree' available as the `evil' undo feature.
+    ;; https://github.com/emacs-evil/evil#dependencies
+    (imp:flag :evil +undo-tree)
 
 
-;;------------------------------------------------------------------------------
-;; The End.
-;;------------------------------------------------------------------------------
-(imp:provide :mantle 'config 'user 'undo-tree)
+    ;;--------------------
+    :custom
+    ;;--------------------
+
+    (undo-tree-visualizer-diff t)
+    (undo-tree-auto-save-history t)
+    (undo-tree-enable-undo-in-region t)
+
+    ;; Increase undo limits to avoid emacs prematurely truncating the undo
+    ;; history and corrupting the tree. Think big.. `undo-tree' history trees
+    ;; consume exponentially more space than linear undo histories, and then
+    ;; some when `undo-tree-enable-undo-in-region' is involved. See
+    ;; syl20bnr/spacemacs#12110
+    (undo-limit        (*  16 1024 1024)) ;  16mb (default is 160kb)
+    (undo-strong-limit (*  64 1024 1024)) ;  64mb (default is 240kb)
+    (undo-outer-limit  (* 256 1024 1024)) ; 256mb (default is 24mb)
+
+
+    ;;--------------------
+    :config
+    ;;--------------------
+
+    ;; Prefer `zstd' for compressing history files.
+    (cond ((executable-find "zstd")
+           (define-advice undo-tree-make-history-save-file-name (:filter-return (file) mantle:advice:compress/zstd)
+             (concat file ".zst")))
+          ((executable-find "gzip")
+           (define-advice undo-tree-make-history-save-file-name (:filter-return (file) mantle:advice:compress/zstd)
+             (concat file ".gz")))
+          ;; Fallback: Just don't compress.
+          (t
+           (nub:debug
+              :innit
+              file/this
+              tags/this
+            "`undo-tree': No compression found for undo history files.")
+           nil))
+
+    ;; Strip text properties from undo-tree data to stave off bloat. File size
+    ;; isn't the concern here; undo cache files bloat easily, which can cause
+    ;; freezing, crashes, GC-induced stuttering or delays when opening files.
+    ;; TODO: Does `nil' for LAMBDA-LIST work for function without params?
+    (define-advice undo-list-transfer-to-tree (:before nil mantle:advice:strip-text-props)
+      (dolist (item buffer-undo-list)
+        (and (consp item)
+             (stringp (car item))
+             (setcar item (substring-no-properties (car item))))))
+
+    ;; Undo-tree is too chatty about saving its history files. This doesn't
+    ;; totally suppress it logging to *Messages*, it only stops it from appearing
+    ;; in the echo-area.
+    (advice-add #'undo-tree-save-history :around #'innit:advice:squelch)
+
+    (global-undo-tree-mode +1))
+
+
+  ;;------------------------------------------------------------------------------
+  ;; The End.
+  ;;------------------------------------------------------------------------------
+  (imp:provide :mantle 'config 'user 'undo-tree)
