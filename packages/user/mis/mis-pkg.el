de28a6bc8e20f2ded21309a6cd3be70fade72b50
Cole Brown <code@brown.dev>
2022-08-08 11:17:30 -0700

    Start of new `mis' package
    Start mis v4.
      - Needed for snippets.
        - Will likely do minimal needed for the snippets to function, to
          start with.
    
    New "mis-pkg.el" to define package
    
    New "mis.el" to load code files (that don't exist yet...).

---
+++ packages/user/mis/mis-pkg.el
@@ -0,0 +1,39 @@
+;;; mis-pkg.el --- Make It So -*- lexical-binding: t; -*-
+;;
+;; Author: Cole Brown <code@brown.dev>
+;; Maintainer: Cole Brown <code@brown.dev>
+;; Created:  2019-10-23
+;; Modified: 2022-08-08
+;; Version:  4.0.0
+;; Keywords: convenience extensions faces
+;; Homepage: https://github.com/work/mis
+;; Package-Requires: ((emacs "28"))
+;;
+;;; Commentary:
+;;
+;; mis ("Make It So") is a rich(er) text cousin to `message'.
+;;
+;;; Code:
+
+
+;;------------------------------------------------------------------------------
+;; Define Package (???)
+;;------------------------------------------------------------------------------
+;; Obsolete in Emacs 28.1, but no documentation to say what to switch to.
+;; Maybe when Emacs 29 comes along and they delete `define-package' there'll be
+;; a replacement? Already `define-package' is just:
+;;   (error "Don't call me")
+;; ...Which isn't very helpful.
+(define-package "mis" "4.0.0"
+  "mis (\"Make It So\") is a rich(er) text cousin to `message'."
+  '((emacs "28"))
+  ;; TODO: Change when mis gets its own repository
+  :homepage "https://github.com/cole-brown/.config-emacs"
+  :keywords '("convenience" "extensions" "faces"))
+
+
+;;------------------------------------------------------------------------------
+;; The End.
+;;------------------------------------------------------------------------------
+(provide 'mis-pkg)
+;;; mis-pkg.el ends here
---
+++ packages/user/mis/mis.el
@@ -0,0 +1,32 @@
+;;; mis.el --- Make It So -*- lexical-binding: t; -*-
+;;
+;; Author: Cole Brown <code@brown.dev>
+;; Maintainer: Cole Brown <code@brown.dev>
+;; Created:  2019-10-23
+;; Modified: 2022-08-08
+;; Version:  4.0.0
+;; Keywords: convenience extensions faces
+;; Homepage: https://github.com/work/mis
+;; Package-Requires: ((emacs "24.3"))
+;;
+;;; Commentary:
+;;
+;; mis ("Make It So") is a rich(er) text cousin to `message'.
+;;
+;;; Code:
+
+
+;;------------------------------------------------------------------------------
+;; Mis Files
+;;------------------------------------------------------------------------------
+
+(require 'mis-style)
+(require 'mis-message)
+(require 'mis-comment)
+
+
+;;------------------------------------------------------------------------------
+;; The End.
+;;------------------------------------------------------------------------------
+(provide 'mis)
+;;; mis.el ends here
