7c44f72f291e6fbbe53cd51f1be0dfb1bd178b8d
Cole Brown <code@brown.dev>
2022-08-16 09:54:40 -0700

    Update `mis:line`
    Update `mis:line` to:
      - be parsed by `int<mis>:parse`
      - return a standardized Mis syntax tree

--- packages/user/mis/mis-art.el
+++ packages/user/mis/mis-art.el
@@ -21,33 +21,59 @@
 ;; Lines
 ;;------------------------------------------------------------------------------
 
-(cl-defun mis:line (&key width string)
-  "Create a line with a maximum WIDTH by repeating STRING characters.
+(defun mis:line (&rest args)
+  "Create a line by repeating a character/string.
 
-WIDTH should be a positive integer.
+ARGS should be a plist of:
+  - Any valid `mis:style' args (e.g. `:width' for a max line width).
+  - `:string' - A string or character to repeat."
+  (let* ((parsed (apply 'int<mis>:parse
+                        'mis:line
+                        '(:line :style)
+                        args))
+         (line (plist-get parsed :line))
+         (string (plist-get line :string))
+         key
+         value
+         ast)
 
-STRING should be a character or a string."
-  ;; TODO: switch to `int<mis>:parse'?
-
-  ;;------------------------------
-  ;; Error Checking
-  ;;------------------------------
-  ;; Also collapse the synonym key params while we're at it.
-  (let ((width (int<mis>:valid:positive-integer? 'mis:line
-                                                 'width
-                                                 width))
-        (string (int<mis>:valid:string-or-char? 'mis:line
-                                                'string
-                                                string)))
+    ;; Must have a string to create the line.
+    (unless string
+      (int<mis>:error 'mis:line
+                      "Invalid ARGS; no `:string' found! %S -> %S"
+                      args
+                      parsed))
 
     ;;------------------------------
     ;; Build & return normalized list.
     ;;------------------------------
-    (list :format 'repeat
-          :width  width
-          :string string)))
+    ;; Convert `:line' from parsing into `:format'.
+    (setq ast (list :format (list :formatter 'repeat
+                                  :string    string)))
+
+    ;; Leave `:style' and anything else as-is.
+    (while parsed
+      (setq key (pop parsed)
+            value (pop parsed))
+
+      (cond ((not (keywordp key))
+             ;; Dunno; error?
+             (int<mis>:error 'mis:line
+                      "Unknown plist key: %S, value: %S"
+                      key
+                      value))
+
+            ((eq key :line)
+             ;; Already dealt with this parsed plist entry.
+             nil)
+
+            ;; Copy into output tree.
+            (t
+             (push value ast)
+             (push key ast))))
+
+    ast))
 ;; (mis:line :width 10 :string "hi")
-;; (mis:line :length 10 :string "hi")
 
 
 ;; TODO: this?    (defun mis:box (&key width corner:top/left corner:top/right corner:bottom/left corner:bottom/right horizontal vertical)
@@ -67,7 +93,6 @@ STRING should be a character or a string."
 ;; (mis:box :width 80 :border 'hypen) ;; or `ascii'?
 ;; (mis:box :width 80 :border 'line)
 ;; (mis:box :width 80 :border 'double)
-
 
 
 ;;------------------------------------------------------------------------------
