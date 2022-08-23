fd8944acf139e7b63c17e964fd76703f9d087bfc
Cole Brown <code@brown.dev>
2022-08-23 09:37:45 -0700

    Rework/update `mis:line`
    Update `mis:line` to work with new alist Mis ASTs.
    
    Use new `int<mis>:syntax:...` helper functions.
    
    Fix implicit string case (`(mis:line "-")`) to work with new
    `:mis:string` as opposed to `:mis:message`, since it did not support
    format-string/args anyways.

--- packages/user/mis/mis-art.el
+++ packages/user/mis/mis-art.el
@@ -15,6 +15,7 @@
 
 (require 'mis-error)
 (require 'mis-valid)
+(require 'mis-parse)
 
 
 ;;------------------------------------------------------------------------------
@@ -32,95 +33,88 @@ ARGS should be a plist of:
 Examples:
   (mis:line :width 10 :string \"xX\")
   (mis:line \"-\")"
-  (let* ((parsed (apply 'int<mis>:parse
-                        'mis:line
-                        '(:line :style)
-                        args))
-         (line (plist-get parsed :line))
-         (string (plist-get line :string))
-         (message (plist-get parsed :message))
+  (let* ((ast/parsed (apply 'int<mis>:parse
+                            'mis:line
+                            '(:line :style)
+                            args))
+         (line/string (int<mis>:syntax:find 'mis:line ast/parsed :tmp:line :string)) ; Explicit string.
+         (mis/string  (int<mis>:syntax:find 'mis:line ast/parsed :mis:string))       ; Implicit string.
          key
          value
-         ast)
+         ast/out)
 
     ;;------------------------------
     ;; String Error Checking / Normalization
     ;;------------------------------
-    ;; Have an explicit `:string' in `:line'?
-    (cond ((not (null string))
-           ;; `:message' not allowed in this case.
-           (when message
+    ;; After this, `line/string' will be used and `mis/string' will be ignored.
+
+    ;; Have an explicit `:string' in `:tmp:line'?
+    (cond ((and (not (null line/string))
+                (not (null mis/string)))
+           ;; `:mis:string' not allowed in this case.
+           (int<mis>:error 'mis:line
+                           "Invalid ARGS; cannot have both `:string' and `:mis:string'."
+                           "`:string': %S, `:mis:string': %S, ARGS: %S"
+                           line/string
+                           mis/string
+                           args))
+
+          ;; No `:string' but do have a `:mis:string'?
+          ((and (null line/string)
+                (not (null mis/string)))
+           ;; Use `:mis:string' as our line's `:string'.
+           (if (or (stringp mis/string)
+                   (characterp mis/string))
+               (setq line/string mis/string)
              (int<mis>:error 'mis:line
-                             "Invalid ARGS; cannot have both `:string' and `:message'."
-                             "`:string': %S, `:message': %S, ARGS: %S"
-                             string
-                             message
+                             '("Invalid ARGS; line string can only be a "
+                               "string or character. "
+                               "Got %S %S, ARGS: %S")
+                             (type-of mis/string)
+                             mis/string
                              args)))
 
-          ;; No `:string' but do have `:message'? Convert `:message' to `:string'.
-          ((and (null string)
-                (not (null message)))
-           ;; Only allow a message that is a char or string without extra args.
-           (cond ((> (length message) 1)
-                  (int<mis>:error 'mis:line
-                                  '("Invalid ARGS; line string can only be a "
-                                    "simple string, not a format string w/ args. "
-                                    "Got: %S, ARGS: %S")
-                                  message
-                                  args))
-                 ((and (not (stringp (car message)))
-                       (not (characterp (car message))))
-                  (int<mis>:error 'mis:line
-                                  '("Invalid ARGS; line string can only be a "
-                                    "string or character. "
-                                    "Got %S %S, ARGS: %S")
-                                  (type-of (car message))
-                                  (car message)
-                                  args))
-                 (t
-                  (setq string (car message)))))
+          ;; `:string' and no `:mis:string'? Ok as-is.
+          ((and (not (null line/string))
+                (null mis/string))
+           nil)
 
-          ;; No `:string', no `:message'...?
+          ;; No `:string', no `:mis:string'...?
           (t
            (int<mis>:error 'mis:line
                            '("Invalid ARGS; line must have a string/character to repeat. "
+                             "Expected `%s' or `%s'. "
                              "Found nothing in ARGS: %S -> %S")
+                           :string
+                           :mis:string
                            args
-                           parsed)))
+                           ast/parsed)))
 
     ;;------------------------------
-    ;; Build & return normalized list.
+    ;; Build & return the AST.
     ;;------------------------------
-    ;; Convert `:line' from parsing into `:format'.
-    (setq ast (list :format (list :formatter 'repeat
-                                  :string    string)))
+    ;; Convert `:tmp:line' from parsing into `:mis:format'.
+    (setq ast/out (int<mis>:syntax:create 'mis:line
+                                          :mis:format
+                                          (cons :formatter 'repeat)
+                                          (cons :string    line/string)))
 
-    ;; Delete `:message' but leave `:style' and anything else as-is.
-    (while parsed
-      (setq key (pop parsed)
-            value (pop parsed))
-
-      (cond ((not (keywordp key))
-             ;; Dunno; error?
-             (int<mis>:error 'mis:line
-                      "Unknown plist key: %S, value: %S"
-                      key
-                      value))
-
-            ((memq key '(:line :message))
-             ;; Already dealt with this parsed plist entry.
-             nil)
-
-            ;; Copy into output tree.
-            (t
-             (push value ast)
-             (push key ast))))
-
-    ast))
+    ;; Add `:style', etc to our output syntax (but ignore message/string).
+    (int<mis>:syntax:merge 'mis:line
+                           ast/out
+                           ast/parsed
+                           :tmp:line :mis:string :mis:message)))
+;; (mis:line :width 10 :string "xX")
+;;   -> '((:mis:style (:width . 10)) (:mis:format (:formatter . repeat) (:string . "xX")))
+;; (mis:line "-")
+;;   -> '((:mis:format (:formatter . repeat) (:string . "-")))
 ;; (mis:line :width 10 :string "hi")
 ;; (mis:line :string "hi")
-;; (mis:line "-")
 
+
+;;------------------------------------------------------------------------------
+;; Boxes
+;;------------------------------------------------------------------------------
 
 ;; TODO: this?    (defun mis:box (&key width corner:top/left corner:top/right corner:bottom/left corner:bottom/right horizontal vertical)
 ;; TODO: or this? (defun mis:box (&key width corners horizontal vertical)
