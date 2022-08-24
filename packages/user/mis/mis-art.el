4f3542fd5421d9940474bf386890e0a84ee29ce5
Cole Brown <code@brown.dev>
2022-08-24 10:45:21 -0700

    Update `mis:line` to use new format function
    `mis:line` now uses `int<mis>:format:syntax` for getting its line
    string, which means it now works for characters too.

--- packages/user/mis/mis-art.el
+++ packages/user/mis/mis-art.el
@@ -33,81 +33,47 @@ ARGS should be a plist of:
 Examples:
   (mis:line :width 10 :string \"xX\")
   (mis:line \"-\")"
-  (let* ((ast/parsed (apply 'int<mis>:parse
-                            'mis:line
-                            '(:line :style)
-                            args))
-         (line/string (int<mis>:syntax:find 'mis:line ast/parsed :tmp:line :string)) ; Explicit string.
-         (mis/string  (int<mis>:syntax:find 'mis:line ast/parsed :mis:string))       ; Implicit string.
-         key
-         value
-         ast/out)
+  (let* ((syntax/parsed (apply 'int<mis>:parse
+                               'mis:line
+                               '(:line :style)
+                               args))
+         (line/string (int<mis>:format:syntax 'mis:line
+                                              syntax/parsed
+                                              '(:tmp:line :string)
+                                              '(tmp:line :char)))
+         syntax/out)
 
     ;;------------------------------
-    ;; String Error Checking / Normalization
+    ;; Error Checking
     ;;------------------------------
-    ;; After this, `line/string' will be used and `mis/string' will be ignored.
 
     ;; Have an explicit `:string' in `:tmp:line'?
-    (cond ((and (not (null line/string))
-                (not (null mis/string)))
-           ;; `:mis:string' not allowed in this case.
-           (int<mis>:error 'mis:line
-                           "Invalid ARGS; cannot have both `:string' and `:mis:string'."
-                           "`:string': %S, `:mis:string': %S, ARGS: %S"
-                           line/string
-                           mis/string
-                           args))
-
-          ;; No `:string' but do have a `:mis:string'?
-          ((and (null line/string)
-                (not (null mis/string)))
-           ;; Use `:mis:string' as our line's `:string'.
-           (if (or (stringp mis/string)
-                   (characterp mis/string))
-               (setq line/string mis/string)
-             (int<mis>:error 'mis:line
-                             '("Invalid ARGS; line string can only be a "
-                               "string or character. "
-                               "Got %S %S, ARGS: %S")
-                             (type-of mis/string)
-                             mis/string
-                             args)))
-
-          ;; `:string' and no `:mis:string'? Ok as-is.
-          ((and (not (null line/string))
-                (null mis/string))
-           nil)
-
-          ;; No `:string', no `:mis:string'...?
-          (t
-           (int<mis>:error 'mis:line
-                           '("Invalid ARGS; line must have a string/character to repeat. "
-                             "Expected `%s' or `%s'. "
-                             "Found nothing in ARGS: %S -> %S")
-                           :string
-                           :mis:string
-                           args
-                           ast/parsed)))
+    (unless line/string
+      (int<mis>:error 'mis:line
+                      '("Invalid ARGS; line must have a string/character to repeat. "
+                        "Found nothing in ARGS: %S -> %S")
+                      args
+                      syntax/parsed))
 
     ;;------------------------------
-    ;; Build & return the AST.
+    ;; Build & return the SYNTAX.
     ;;------------------------------
     ;; Convert `:tmp:line' from parsing into `:mis:format'.
-    (setq ast/out (int<mis>:syntax:create 'mis:line
-                                          :mis:format
-                                          (cons :formatter 'repeat)
-                                          (cons :string    line/string)))
+    (setq syntax/out (int<mis>:syntax:create 'mis:line
+                                             :mis:format
+                                             (cons :formatter 'repeat)
+                                             (cons :string    line/string)))
 
     ;; Add `:style', etc to our output syntax (but ignore message/string).
     (int<mis>:syntax:merge 'mis:line
-                           ast/out
-                           ast/parsed
-                           :tmp:line :mis:string :mis:message)))
+                           syntax/out
+                           syntax/parsed
+                           :tmp:line :mis:char :mis:string :mis:message)))
 ;; (mis:line :width 10 :string "xX")
 ;;   -> '((:mis:style (:width . 10)) (:mis:format (:formatter . repeat) (:string . "xX")))
 ;; (mis:line "-")
 ;;   -> '((:mis:format (:formatter . repeat) (:string . "-")))
+;; (mis:line ?-)
 ;; (mis:line :width 10 :string "hi")
 ;; (mis:line :string "hi")
 
