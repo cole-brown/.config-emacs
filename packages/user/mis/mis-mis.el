aea7678338f45abe69a953cf1878687b7badccea
Cole Brown <code@brown.dev>
2022-10-03 10:49:58 -0700

    Fix buffer context for Mis.
    Fix `mis` to enter the desired output buffer's context before doing
    anything (parsing, compiling, etc) so that so that everything is
    parsed/compiled/etc with the correct values/settings/etc available.
      - Change `mis` from func to macro so it can control when things are
        evaluated.
        - Look through inputs for `:buffer` kvp first.

--- packages/user/mis/mis-mis.el
+++ packages/user/mis/mis-mis.el
@@ -27,19 +27,106 @@
 ;; Output API
 ;;------------------------------------------------------------------------------
 
-(defun mis (&rest args)
-  "Output a message built from ARGS."
-  (let (styling
+(defmacro mis (&rest args)
+  "Output a message built from ARGS.
+
+Top-Level Keywords:
+  - `:buffer'
+    - Something `int<mis>:buffer:name' can understand like:
+      - nil
+        - Send to `mis:buffer:default'.
+      - `:messages', `messages'
+        - Send to '*Messages*' buffer.
+      - `:message', `message'
+        - Send to '*Messages*' buffer.
+      - `:mis', `mis'
+        - Send to `mis:buffer:name' buffer.
+      - A string.
+        - Send to the buffer name in the string.
+
+Rest of the args: Probably Mis function calls.
+  - TODO: Lots of documentation in here?"
+  (let ((macro:keywords/expected '(:buffer))
+        macro:key
+        macro:buffer
+        macro:rest
+        macro:metatree)
+    (dolist (arg args)
+      (if (memq arg macro:keywords/expected)
+          (setq macro:key arg) ; Save keyword.
+        (if macro:key
+            (progn
+              ;; Get keyword's value into the correct variable.
+              (cond ((eq macro:key :buffer)
+                     (setq macro:buffer arg))
+                    (t
+                     (int<mis>:error 'mis
+                                     "Unhandled top-level key/value: %S = %S"
+                                     macro:key
+                                     arg)))
+              (setq macro:key nil))
+          ;; Else it's something else and we'll deal with it after sorting the args.
+          (push arg macro:rest))))
+
+    ;; `(message "buffer:   %S" ,macro:buffer)
+
+    ;; Finalize the parsed parameters.
+    (setq macro:rest     (nreverse macro:rest)
+          macro:metatree `(int<mis>:buffer:metadata 'mis ,macro:buffer nil)
+          macro:buffer   `(int<mis>:output/metadata:find 'mis
+                                                         :buffer:object
+                                                         ,macro:metatree))
+
+    ;; `(message "buffer:   %S" ,macro:buffer)
+    ;; `(message "metatree: %S" ,macro:metatree)
+    ;; `(message "rest:     %S" ',macro:rest)
+
+    ;; So far, none of the `mis:___' parser/compiler calls has actually been
+    ;; evaluated and we got our top-level args like `buffer'. This is necessary
+    ;; as we need to be in the buffer's context before evaluating anything so
+    ;; that widths and styles and such are determined correctly.
+    `(with-current-buffer ,macro:buffer
+       (save-mark-and-excursion
+         ;; In our buffer now... So evalute and resolve things.
+         (int<mis>:mis 'mis
+                       ,macro:metatree
+                       ;; Parsing happens when these are evaluated, before
+                       ;; `int<mis>:mis' is called.
+                       ,@macro:rest)))))
+;; (mis :buffer 'foo
+;;      (message "hello there"))
+
+
+(defun int<mis>:mis (caller metatree &rest args)
+  "Output a message built from ARGS to BUFFER.
+
+Caller is expected to have switched to the context of BUFFER already.
+
+ARGS should be Mis Output Trees. That is, it should have been Mis API calls
+\(e.g. `mis:string') that have been evaluated already.
+
+METATREE should be nil or a Mis Output Tree of top-level metadata like:
+  - `:buffer:name'
+  - `:buffer:object'
+
+BUFFER should be the buffer object.
+
+CALLER should be calling function's name. It can be one of:
+  - a string
+  - a quoted symbol
+  - a function-quoted symbol
+  - a list of the above, most recent first
+    - e.g. '(#'error-caller \"parent\" 'grandparent)"
+  (let ((caller (list 'int<mis>:mis caller))
+        styling
         content
-        output
-        buffer
-        metadata-mot) ;; Mis Output Tree of just (top-level) metadata.
+        output)
 
     ;;------------------------------
     ;; Parsing & Validation
     ;;------------------------------
-    ;; Check for validity, duplicate styles. Also look for any options we need
-    ;; to skim off (e.g. `:buffer').
+    ;; Check for validity, duplicate styles. Also look for any options that
+    ;; should have already been skimmed off into METATREE.
     (while args
       (let ((arg (pop args)))
         ;; Is this non-nil and /only/ styling info? Then it's global styling for
@@ -47,14 +134,14 @@
         (cond ((and arg
                     (int<mis>:style:exclusive? arg))
                ;; Make sure it's valid...ish.
-               (int<mis>:valid:syntax? 'mis 'arg arg)
+               (int<mis>:valid:syntax? caller 'arg arg)
 
                (if styling
                    ;; There can be only one.
                    ;;
                    ;; NOTE: We could allow more than one and merge, but then
                    ;; we'd have to do collision detection...
-                   (int<mis>:error 'mis
+                   (int<mis>:error caller
                                    '("Only one Mis `:style' allowed per level in ARGS. "
                                      "have: %S, found: %S, args: %S")
                                    styling
@@ -65,117 +152,62 @@
 
               ((listp arg)
                ;; Think this is a Mis syntax tree? Should be anyways; error if not.
-               (int<mis>:valid:syntax? 'mis 'arg arg)
+               (int<mis>:valid:syntax? caller 'arg arg)
                ;; Save off this mis content type (messages, comments, arts...).
                (push arg content))
 
               ;; Top-level arg? Example: `:buffer'
+              ;; Should have already been delt with and put into METATREE, so...
+              ;; Error!
               ((keywordp arg)
                (let ((key arg)
                      (value (pop arg)))
-                 ;; Put top-level Mis arg into top-level metadata.
-                 (pcase key
-                   (:buffer
-                    (if buffer
-                        (int<mis>:error 'mis
-                                        '("Only one Mis `:buffer' allowed. "
-                                          "have: %S, found: %S, args: %S")
-                                        buffer
-                                        value
-                                        args)
-                      ;; Validate value later (when used).
-                      (setq buffer value)))
-
-                   ;; TODO: Other top-level keywords?
-
-                   (_
-                    (int<mis>:error 'mis
-                                    '("Unknown/unhandled args keyword/value pair!"
-                                      "unknown keyword: %S, value: %S, args: %S")
-                                    key
-                                    value
-                                    args)))))
+                 (int<mis>:error caller
+                                 '("Top-Level keywords should have already been parsed! "
+                                   "found: %S = %S")
+                                 key
+                                 value)))
 
               ;; Fallthrough: Error!
               (t
-               (int<mis>:error 'mis
+               (int<mis>:error caller
                                '("Unknown/unhandled argument!"
                                  "unknown: %S, args: %S")
                                arg
                                args)))))
 
     ;;------------------------------
-    ;; Context of Target Buffer
+    ;; Compile Mis syntax into strings.
     ;;------------------------------
-    ;;
-    ;; First, get the buffer name & object from... whichever we do or do not
-    ;; have right now.
-    (setq metadata-mot (int<mis>:output:update/metadata
-                        caller
-                        metadata-mot
-                        (int<mis>:buffer:metadata caller
-                                                  buffer
-                                                  metadata-mot))
-          ;; Now change `buffer' from "dunno... name, object, nil maybe?" to "def. object".
-          buffer (int<mis>:output/metadata:find caller
-                                                :buffer:object
-                                                metadata-mot))
+    ;; Compile each parsed Mis Syntax Tree in `content' in current (reverse)
+    ;; order & push to compiled Mis Output Trees to `output' list. Final step will then have `output' in
+    ;; forwards order.
+    (while content
+      (int<mis>:debug caller
+                      "compile: %S"
+                      (nth 0 content))
+      ;; Get a syntax tree; turn into an output tree.
+      (push (int<mis>:compile caller
+                              (pop content)
+                              styling)
+            output))
 
-    ;; We need to be in the context of the target buffer while parsing,
-    ;; compiling, etc... So now that we know what the target buffer is, switch
-    ;; to be in it.
-    (with-current-buffer buffer
+    (int<mis>:debug caller
+                    "compiled output: %S"
+                    output)
 
-      ;;------------------------------
-      ;; Compile Mis syntax into strings.
-      ;;------------------------------
-      ;; Compile each piece of `content' in current (reverse) order & push to
-      ;; `output' list. Final step will then have `output' in forwards order.
-      (while content
-        (int<mis>:debug 'mis
-                        "compile: %S"
-                        (nth 0 content))
-        ;; Get a syntax tree; turn into a string.
-        (push (int<mis>:compile 'mis
-                                (pop content)
-                                styling)
-              output))
-
-      (int<mis>:debug 'mis
-                      "compiled output: %S"
-                      output)
-
-      ;;------------------------------
-      ;; Print Output
-      ;;------------------------------
-      ;; Now we should only have outputs left; print 'em.
-      (int<mis>:print 'mis
-                      ;; Our top-level metadata like `:buffer'.
-                      (int<mis>:output:create caller
-                                              nil
-                                              metadata)
-                      output))))
+    ;;------------------------------
+    ;; Print Output
+    ;;------------------------------
+    ;; Now we should only have outputs left; print 'em.
+    (int<mis>:print caller
+                    ;; Our top-level metadata like `:buffer'.
+                    metatree
+                    ;; Our list of Mis Output Trees.
+                    (nreverse output))))
 ;; (mis
 ;;  (mis:style :width 80)
 ;;  (mis:line "-"))
-;; (mis (mis:comment (mis:line "-")))
-;; (mis
-;;   (mis:style :width 80)
-;;   (mis:comment (mis:line "-"))
-;;   (mis:comment "Hello there.")
-;;   (mis:comment (mis:line "-")))
-;; TODO-mis: YOU ARE HERE:
-;; (mis (mis:comment (mis:string :align 'center "Hello there.")))
-;; (mis
-;;   (mis:style :width 80)
-;;   (mis:comment (mis:line "-")
-;;                (mis:string :align 'center "Hello there.")
-;;                (mis:line "-")))
-;; (mis
-;;   (mis:comment :width 80
-;;                (mis:line "-")
-;;                (mis:string :align 'center "Hello there.")
-;;                (mis:line "-")))
 
 
 ;;------------------------------------------------------------------------------
