a78da9249562b950c6c1a553575f77b256d75f0a
Cole Brown <code@brown.dev>
2022-11-08 11:11:12 -0800

    Eval signature keybinds after evil
    Add explicit `imp:eval:after` to "mantle/config/keybinds/signature.el"
    so it can eval after evil or not, depending on what it wants to do.

--- mantle/config/keybinds/signature.el
+++ mantle/config/keybinds/signature.el
@@ -13,89 +13,98 @@
 
 
 (imp:require :elisp 'utils 'functions)
+(imp:require :tools 'signature)
+
 
 
 ;;------------------------------------------------------------------------------
 ;; Keybinds
 ;;------------------------------------------------------------------------------
 
-;; NOTE: /mantle/config/keyboard.el has the new general defs
-;; NOTE: /legacy/sn003-doom/config/keybinds/spy-leader.el has the old shit
+;; TODO: Evil vs Emacs keybinds?
+
+;; After evil loads:
+(imp:eval:after (:and evil evil-collection)
+
+  ;; NOTE: /mantle/config/keyboard.el has the new general defs
+  ;; NOTE: /legacy/sn003-doom/config/keybinds/spy-leader.el has the old shit
 
 
- ;;------------------------------
- ;; Signatures
- ;;------------------------------
+  ;;------------------------------
+  ;; Signatures
+  ;;------------------------------
 
-(keybind:leader/global:def
- :infix (keybind:infix "i" "s")      ;; insert -> signature
- "" '(nil :which-key "Signature...") ;; Infix Title
-
- ;; "TODO" signature with timestamp; commented if needed:
- "t" (list (elisp:cmd (signature:insert 'sigil 'todo :timestamp t :comment t))
-           :which-key (concat "TODO: " (signature:string 'sigil 'todo)))
-
- ;; Sigil for Note Prefix.
- "n" (list (elisp:cmd (signature:insert 'sigil 'note))
-           :which-key (concat "Note: " (signature:string 'sigil 'note)))
-
-
- ;; Signature for the end of an email or something.
- "s" (list (elisp:cmd (signature:insert 'name 'sign))
-           :which-key (concat "Sign: " (signature:string 'name 'sign))))
-
-(keybind:leader/global:def
- :infix (keybind:infix "i" "s" "i")      ;; insert -> signature -> ID
- "" '(nil :which-key "ID...") ;; Infix Title
-
- ;; Just the Sigil.
- "s" (list (elisp:cmd (signature:insert 'id 'sigil))
-           :which-key (concat "Sigil: " (signature:string 'id 'sigil)))
-
- ;; Just the Name.
- "n" (list (elisp:cmd (signature:insert 'id 'name))
-           :which-key (concat "Name: " (signature:string 'id 'name))))
-
-;;------------------------------
-;; Emails
-;;------------------------------
-;; Only show if we have any.
-
-;; work namespace.
-(when (signature:exists? 'id 'email :namespace :work)
   (keybind:leader/global:def
-   :infix (keybind:infix "i" "s" "e") ;; insert -> signature -> email
-   "" '(nil :which-key "Email...")    ;; Infix Title
+   :infix (keybind:infix "i" "s")      ;; insert -> signature
+   "" '(nil :which-key "Signature...") ;; Infix Title
 
-   "w" (list (elisp:cmd (signature:insert 'id 'email :namespace :work))
-             :which-key (concat "work: " (signature:string 'id 'email :namespace :work)))))
+   ;; "TODO" signature with timestamp; commented if needed:
+   "t" (list (elisp:cmd (signature:insert 'sigil 'todo :timestamp t :comment t))
+             :which-key (concat "TODO: " (signature:string 'sigil 'todo)))
 
-   ;; home namespace.
-(when (signature:exists? 'id 'email :namespace :home)
+   ;; Sigil for Note Prefix.
+   "n" (list (elisp:cmd (signature:insert 'sigil 'note))
+             :which-key (concat "Note: " (signature:string 'sigil 'note)))
+
+
+   ;; Signature for the end of an email or something.
+   "s" (list (elisp:cmd (signature:insert 'name 'sign))
+             :which-key (concat "Sign: " (signature:string 'name 'sign))))
+
   (keybind:leader/global:def
-   :infix (keybind:infix "i" "s" "e") ;; insert -> signature -> email
-   ;; TODO: Do I need to redefine the title?
-   ;; "" '(nil :which-key "Email...")    ;; Infix Title
+   :infix (keybind:infix "i" "s" "i")      ;; insert -> signature -> ID
+   "" '(nil :which-key "ID...") ;; Infix Title
 
-   "h" (list (elisp:cmd (signature:insert 'id 'email :namespace :home))
-             :which-key (concat "home: " (signature:string 'id 'email :namespace :home)))))
+   ;; Just the Sigil.
+   "s" (list (elisp:cmd (signature:insert 'id 'sigil))
+             :which-key (concat "Sigil: " (signature:string 'id 'sigil)))
 
-   ;; default namespace.
-(when (signature:exists? 'id 'email :namespace :default)
-  (keybind:leader/global:def
-   :infix (keybind:infix "i" "s" "e") ;; insert -> signature -> email
-   ;; TODO: Do I need to redefine the title?
-   ;; "" '(nil :which-key "Email...")    ;; Infix Title
+   ;; Just the Name.
+   "n" (list (elisp:cmd (signature:insert 'id 'name))
+             :which-key (concat "Name: " (signature:string 'id 'name))))
 
-   "c" (list (elisp:cmd (signature:insert 'id 'email :namespace :default))
-                :which-key (concat "default: " (signature:string 'id 'email :namespace :default)))))
+  ;;------------------------------
+  ;; Emails
+  ;;------------------------------
+  ;; Only show if we have any.
+
+  ;; work namespace.
+  (when (signature:exists? 'id 'email :namespace :work)
+    (keybind:leader/global:def
+     :infix (keybind:infix "i" "s" "e") ;; insert -> signature -> email
+     "" '(nil :which-key "Email...")    ;; Infix Title
+
+     "w" (list (elisp:cmd (signature:insert 'id 'email :namespace :work))
+               :which-key (concat "work: " (signature:string 'id 'email :namespace :work)))))
+
+  ;; home namespace.
+  (when (signature:exists? 'id 'email :namespace :home)
+    (keybind:leader/global:def
+     :infix (keybind:infix "i" "s" "e") ;; insert -> signature -> email
+     ;; TODO: Do I need to redefine the title?
+     ;; "" '(nil :which-key "Email...")    ;; Infix Title
+
+     "h" (list (elisp:cmd (signature:insert 'id 'email :namespace :home))
+               :which-key (concat "home: " (signature:string 'id 'email :namespace :home)))))
+
+  ;; default namespace.
+  (when (signature:exists? 'id 'email :namespace :default)
+    (keybind:leader/global:def
+     :infix (keybind:infix "i" "s" "e") ;; insert -> signature -> email
+     ;; TODO: Do I need to redefine the title?
+     ;; "" '(nil :which-key "Email...")    ;; Infix Title
+
+     "c" (list (elisp:cmd (signature:insert 'id 'email :namespace :default))
+               :which-key (concat "default: " (signature:string 'id 'email :namespace :default)))))
 
 
-;;------------------------------
-;; Search
-;;------------------------------
+  ;;------------------------------
+  ;; Search
+  ;;------------------------------
 
-;; TODO: Search for: sigil, todo...
+  ;; TODO: Search for: sigil, todo...
+
+)
 
 
 ;;------------------------------------------------------------------------------
