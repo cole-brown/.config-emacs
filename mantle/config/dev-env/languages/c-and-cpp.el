c4848f3fdc7edecd689d7097fa19b54c39f0a249
Cole Brown <code@brown.dev>
2022-12-12 10:18:04 -0800

    Move C/C++ config from legacy

--- mantle/config/dev-env/languages/c-and-cpp.el
+++ mantle/config/dev-env/languages/c-and-cpp.el
@@ -1,4 +1,21 @@
-;;; config/code/+c.el -*- lexical-binding: t; -*-
+;;; mantle/config/dev-env/languages/c-and-cpp.el --- Config the C. -*- lexical-binding: t; -*-
+;;
+;; Author:     Cole Brown <http://github/cole-brown>
+;; Maintainer: Cole Brown <code@brown.dev>
+;; Created:    20
+;; Modified:   2022-12-12
+;; URL:        https://github.com/cole-brown/.config-emacs
+;;
+;; These are not the GNU Emacs droids you're looking for.
+;; We can go about our business.
+;; Move along.
+;;
+;;; Commentary:
+;;
+;;  Config the C.
+;;  And the C++.
+;;
+;;; Code:
 
 ;;------------------------------
 ;; NOTE:
