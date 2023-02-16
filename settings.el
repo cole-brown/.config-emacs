99e3f92b710d8cbfee94af31a27eb7acbd5548fc
Cole Brown <code@brown.dev>
2023-02-16 13:01:34 -0800

    Switch to meow in settings.el

--- settings.el
+++ settings.el
@@ -70,9 +70,9 @@
 (imp:flag :keyboard +dvorak)
 
 ;; Used to decide what, if any, input systems to configure.
-;; TODO-meow: NO MORE EVIL!
-(imp:flag :keybinds +evil) ; https://github.com/emacs-evil/evil
-;; (imp:flag :keybinds +meow)    ; https://github.com/meow-edit/meow
+;; (imp:flag :keybinds +evil) ; https://github.com/emacs-evil/evil
+(imp:flag :keybinds +meow)    ; https://github.com/meow-edit/meow
+
 
 ;;------------------------------
 ;; Automatic
