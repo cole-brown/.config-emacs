e67cb6222755095aecadc76aafc0f489d7560379
Cole Brown <code@brown.dev>
2023-03-08 12:12:36 -0800

    Update buffer region helpers for meow.
    Update existing `buffer:region:...` functions to work with `meow`.
    
    Add a few more helper that also work with `meow`.

--- core/modules/emacs/buffer/region.el
+++ core/modules/emacs/buffer/region.el
@@ -17,33 +17,91 @@
 ;;; Code:
 
 
+(require 'cl-lib)
+
+
 ;;------------------------------------------------------------------------------
 ;; Predicates
 ;;------------------------------------------------------------------------------
 
 (defun buffer:region:active? ()
   "Return non-nil if selection is active.
-Will detect 'evil' visual mode as well.
-TODO-meow: Anything special for meow?"
+Will detect 'evil'/'meow' modal keybind packages and return appropriately."
   (declare (side-effect-free t))
-  (or (use-region-p)
-      (and (bound-and-true-p evil-local-mode)
-           (evil-visual-state-p))))
+  (cond
+   ;;------------------------------
+   ;; `meow'?
+   ;;------------------------------
+   ((bound-and-true-p meow-mode)
+    ;; Need to also be in the right meow mode then.
+    (and (bound-and-true-p meow-normal-mode)
+         (use-region-p)))
+
+   ;;------------------------------
+   ;; `evil'?
+   ;;------------------------------
+   ((bound-and-true-p evil-mode)
+    (and (bound-and-true-p evil-local-mode)
+         (evil-visual-state-p)))
+
+   ;;------------------------------
+   ;; Vanilla Emacs?
+   ;;------------------------------
+   (t
+    (use-region-p))))
 
 
 ;;------------------------------------------------------------------------------
 ;; Region Helpers
 ;;------------------------------------------------------------------------------
 
-(defun buffer:region:beginning ()
+(defun buffer:region:min (start end)
+  "Return the smaller of START and END."
+  (min start end))
+
+
+(defun buffer:region:max (start end)
+  "Return the larger of START and END."
+  (max start end))
+
+
+(defun buffer:region:start ()
   "Return beginning position of selection.
 Use `evil-visual-beginning' if available.
 TODO-meow: Anything special for meow?"
   (declare (side-effect-free t))
-  (or (and (bound-and-true-p evil-local-mode)
-           (markerp evil-visual-beginning)
-           (marker-position evil-visual-beginning))
-      (region-beginning)))
+  (cond
+   ;;------------------------------
+   ;; `meow'?
+   ;;------------------------------
+   ((bound-and-true-p meow-mode)
+    ;; https://github.com/meow-edit/meow/blob/master/TUTORIAL.org#states
+    ;; Need to also be in the right meow mode then... but which are "right"?
+    ;;   - INSERT - Wrong; writing things.
+    ;;   - NORMAL - Right; keybinds love being used here.
+    ;;   - MOTION - Right; default ("normal") state for modes that don't insert, like `dired', `proced'.
+    ;;   - KEYPAD - Right? Leader key's temporary state while figuring out its keybind.
+    ;;   - BEACON - Wrong? For applying macros / kinda like multiple-cursors.
+    (and (or (meow-normal-mode-p)
+             (meow-motion-mode-p)
+             (meow-keypad-mode-p))
+        (region-beginning)))
+
+   ;;------------------------------
+   ;; `evil'?
+   ;;------------------------------
+   ((bound-and-true-p evil-local-mode)
+    (and (markerp evil-visual-beginning)
+         (marker-position evil-visual-beginning)))
+
+   ;;------------------------------
+   ;; Vanilla Emacs?
+   ;;------------------------------
+   (t
+    (region-beginning))))
+
+
+(defalias 'buffer:region:beginning 'buffer:region:start)
 
 
 (defun buffer:region:end ()
@@ -51,9 +109,46 @@ TODO-meow: Anything special for meow?"
 Use `evil-visual-end' if available.
 TODO-meow: Anything special for meow?"
   (declare (side-effect-free t))
-  (if (bound-and-true-p evil-local-mode)
-      evil-visual-end
-    (region-end)))
+  (cond
+   ;;------------------------------
+   ;; `meow'?
+   ;;------------------------------
+   ((bound-and-true-p meow-mode)
+    ;; https://github.com/meow-edit/meow/blob/master/TUTORIAL.org#states
+    ;; Need to also be in the right meow mode then... but which are "right"?
+    ;;   - INSERT - Wrong; writing things.
+    ;;   - NORMAL - Right; keybinds love being used here.
+    ;;   - MOTION - Right; default ("normal") state for modes that don't insert, like `dired', `proced'.
+    ;;   - KEYPAD - Right? Leader key's temporary state while figuring out its keybind.
+    ;;   - BEACON - Wrong? For applying macros / kinda like multiple-cursors.
+    (and (or (meow-normal-mode-p)
+             (meow-motion-mode-p)
+             (meow-keypad-mode-p))
+        (region-end)))
+
+   ;;------------------------------
+   ;; `evil'?
+   ;;------------------------------
+   ((bound-and-true-p evil-local-mode)
+    evil-visual-end)
+
+   ;;------------------------------
+   ;; Vanilla Emacs?
+   ;;------------------------------
+   (t
+    (region-end))))
+
+
+(cl-defun buffer:region:get (&key start end properties?)
+  "Return the active region as a string or nil if no active region."
+  ;; Checks & stuff. `nil' for these means don't do something.
+  (when-let* ((active? (buffer:region:active?))
+              (start   (or start (buffer:region:start)))
+              (end     (or end   (buffer:region:end))))
+    (funcall (if properties?
+                 #'buffer-substring
+               #'buffer-substring-no-properties)
+             start end)))
 
 
 ;;------------------------------------------------------------------------------
