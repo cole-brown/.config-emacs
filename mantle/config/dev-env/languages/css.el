a2a70bd48d0e13619f4cefb15d1dfe2153bc335f
Cole Brown <code@brown.dev>
2023-03-16 10:21:22 -0700

    Fix missing functions
    Add `mantle:css:comment-indent-new-line` and
    `mantle:css:fill/adaptive` functions to replace 2 Doom `+css...`
    functions that didn't get replaced when doing "css.el".

--- mantle/config/dev-env/languages/css.el
+++ mantle/config/dev-env/languages/css.el
@@ -28,6 +28,70 @@
   :init
   ;;------------------------------
 
+  (defun mantle:css:comment-indent-new-line (&optional _)
+    "Continues the comment in an indented new line.
+
+Meant for `comment-line-break-function' in `css-mode' and `scss-mode'.
+
+From Doom's `+css/comment-indent-new-line' in \"modules/lang/web/autoload/css.el\"."
+    (interactive)
+    (cond ((or (not (buffer:point:inside-comment?))
+               (and comment-use-syntax
+                    (not (save-excursion (comment-beginning)))))
+           (let (comment-line-break-function)
+             (newline-and-indent)))
+
+          ((save-match-data
+             (let ((at-end (looking-at-p ".+\\*/"))
+                   (indent-char (if indent-tabs-mode ?\t ?\s))
+                   (post-indent (save-excursion
+                                  (move-to-column (1+ (current-indentation)))
+                                  (skip-chars-forward " \t" (line-end-position))))
+                   (pre-indent (current-indentation))
+                   opener)
+               (save-excursion
+                 (if comment-use-syntax
+                     (goto-char (comment-beginning))
+                   (goto-char (line-beginning-position))
+                   (when (re-search-forward comment-start-skip (line-end-position) t)
+                     (goto-char (or (match-end 1)
+                                    (match-beginning 0)))))
+                 (buffer-substring-no-properties (point) (line-end-position))
+                 (when (looking-at "\\(//\\|/?\\*\\**/?\\)\\(?:[^/]\\)")
+                   (list (match-string-no-properties 1)
+                         (- (match-beginning 1) (line-beginning-position))))
+                 (if (looking-at "\\(//\\|/?\\*\\**/?\\)\\(?:[^/]\\)")
+                     (setq opener (match-string-no-properties 1)
+                           pre-indent (- (match-beginning 1) (line-beginning-position)))
+                   (setq opener ""
+                         pre-indent 0)))
+               (insert-and-inherit
+                "\n" (make-string pre-indent indent-char)
+                (if (string-prefix-p "/*" opener)
+                    (if (or (eq +web-continue-block-comments t)
+                            (string= "/**" opener))
+                        " *"
+                      "")
+                  opener)
+                (make-string post-indent indent-char))
+               (when at-end
+                 (save-excursion
+                   (just-one-space)
+                   (insert "\n" (make-string pre-indent indent-char)))))))))
+
+
+  (defun mantle:css:fill/adaptive ()
+    "An `adaptive-fill-function' that conjoins SCSS line comments correctly.
+
+From Doom's `+css-adaptive-fill-fn' in \"modules/lang/web/autoload/css.el\"."
+    (when (looking-at "[ \t]*/[/*][ \t]*")
+      (let ((str (match-string 0)))
+        (when (string-match "/[/*]" str)
+          (replace-match (if (string= (match-string 0 str) "/*")
+                             " *"
+                           "//")
+                         t t str)))))
+
   ;; Switch between files with the same basename and different extensions.
   (imp:eval:after projectile
    (dolist (other-files '(("css"  "scss" "sass" "less" "styl")
@@ -44,9 +108,9 @@
       :file   macro<imp>:path/file
       :docstr "Settings for CSS mode. Non-LSP stuff.")
    ;; Correctly continue /* and // comments on newline-and-indent
-   (setq comment-line-break-function #'+css/comment-indent-new-line)
+   (setq comment-line-break-function #'mantle:css:comment-indent-new-line)
    ;; Fix `fill-paragraph' not conjoining line comments in CSS modes correctly.
-   (setq adaptive-fill-function #'+css-adaptive-fill-fn)
+   (setq adaptive-fill-function #'mantle:css:fill/adaptive)
    ;; Fix filled lines not being auto-prefixed with a * when needed.
    (setq adaptive-fill-first-line-regexp "\\'[ \t]*\\(?:\\* *\\)?\\'")
 
