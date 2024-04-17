946692125d90b07193372e50be1b712a471190ee
Cole Brown <code@brown.dev>
2024-04-17 10:52:03 -0700

    window keybinds: fix split v/h; add resize

--- mantle/config/keybinds/window.el
+++ mantle/config/keybinds/window.el
@@ -35,22 +35,10 @@
       "" '(nil :which-key "Window...")) ; infix title
 
     ;;------------------------------
-    ;; Split
-    ;;------------------------------
-    (keybind:leader/global:def
-      :infix (keybind:infix "w" "s")  ; window -> split
-      "" '(nil :which-key "Split...") ; infix title
-
-      "h" (list #'split-window-right :which-key "◑ Split: Horizontal ")
-      "v" (list #'split-window-below :which-key "◒ Split: Vertical")
-      ;; "w ss" for default desired split: vertical
-      "s" (list #'split-window-below :which-key "◒ Split: Vertical"))
-
-    ;;------------------------------
     ;; Movement - ↑←↓→
     ;;------------------------------
     (keybind:leader/global:def
-      :infix (keybind:infix "w")  ; window
+      :infix (keybind:infix "w") ; window
 
       ;; "ww" for default desired move: "other" window
       "w" (list #'other-window   :which-key "↔ Move: Other")
@@ -60,6 +48,39 @@
       "o" (list #'windmove-left  :which-key "← Move: Left")
       "e" (list #'windmove-down  :which-key "↓ Move: Down")
       "u" (list #'windmove-right :which-key "→ Move: Right"))
+
+    ;;------------------------------
+    ;; Split
+    ;;------------------------------
+    (keybind:leader/global:def
+      :infix (keybind:infix "w") ; window
+
+      "v" (list #'split-window-below :which-key "◒ Split: Vertical")
+      "h" (list #'split-window-right :which-key "◑ Split: Horizontal")
+      ;; "w s" for default desired split: vertical
+      "s" (list #'split-window-right :which-key "◑ Split: Horizontal"))
+
+    ;;------------------------------
+    ;; Resize
+    ;;------------------------------
+    ;; In main "Window" shortcuts
+    (keybind:leader/global:def
+      :infix (keybind:infix "w") ; window
+
+      "=" (list #'balance-windows :which-key "Size: Balance"))
+
+    ;; In "Window -> Resize" shortcuts
+    (keybind:leader/global:def
+      :infix (keybind:infix "w r")     ; window -> resize
+      "" '(nil :which-key "Resize...") ; infix title
+
+      "=" (list #'balance-windows     :which-key "Resize: Balance")
+
+      ;; ↑←↓→
+      "." (list #'enlarge-window              :which-key "↑ Resize: Taller")
+      "o" (list #'shrink-window-horizontally  :which-key "← Resize: Skinnier")
+      "e" (list #'shrink-window               :which-key "↓ Resize: Shorter")
+      "u" (list #'enlarge-window-horizontally :which-key "→ Resize: Wider"))
 
     ;;------------------------------
     ;; Delete
