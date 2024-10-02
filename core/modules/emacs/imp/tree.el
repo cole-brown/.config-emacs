a4ba59f3e7ef52c5dd5439a79955c9a832887a41
Cole Brown <code@brown.dev>
2024-10-02 13:13:21 -0700

    Remove unnecessary `require`
    Was a start up issue on a new Ubuntu install.

--- core/modules/emacs/imp/tree.el
+++ core/modules/emacs/imp/tree.el
@@ -4,7 +4,7 @@
 ;; Maintainer: Cole Brown <code@brown.dev>
 ;; URL:        https://github.com/cole-brown/.config-emacs
 ;; Created:    2023-06-22
-;; Timestamp:  2023-06-22
+;; Timestamp:  2024-10-02
 ;;
 ;; These are not the GNU Emacs droids you're looking for.
 ;; We can go about our business.
@@ -21,9 +21,6 @@
 ;;                                 ──────────
 ;;
 ;;; Code:
-
-
-(require 'seq)
 
 
 ;;--------------------------------------------------------------------------------
